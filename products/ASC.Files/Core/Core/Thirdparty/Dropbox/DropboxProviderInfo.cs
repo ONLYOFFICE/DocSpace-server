4ab7d4db202a8d378ce7ae2c0f4fceb7c4a77b1a
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2023-05-29 14:28:15 +0300

    Files: DropboxProviderInfo: added new property

--- products/ASC.Files/Core/Core/Thirdparty/Dropbox/DropboxProviderInfo.cs
+++ products/ASC.Files/Core/Core/Thirdparty/Dropbox/DropboxProviderInfo.cs
@@ -32,6 +32,7 @@ internal class DropboxProviderInfo : AbstractProviderInfo<FileMetadata, FolderMe
 {
     public override Selector Selector { get; } = Selectors.Dropbox;
     public override ProviderFilter ProviderFilter { get; } = ProviderFilter.DropBox;
+    public override bool MutableEntityId => true;
 
     public DropboxProviderInfo(DisposableWrapper wrapper, ProviderInfoHelper providerInfoHelper) : base(wrapper, providerInfoHelper)
     {
