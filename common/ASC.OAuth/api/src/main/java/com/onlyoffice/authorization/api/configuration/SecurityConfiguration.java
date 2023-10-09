49f2b0b05933515fa6b1d6cceaac50cd4cc8cd61
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-10-09 17:23:48 +0500

    fix: secure api endpoints

---
+++ common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/configuration/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.onlyoffice.authorization.api.configuration;
+
+import com.onlyoffice.authorization.api.filters.CheckAuthCookieFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final CheckAuthCookieFilter cookieFilter;
+    @Bean
+    SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll())
+                .addFilterAt(cookieFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(c -> c.disable())
+                .cors(c -> c.disable())
+                .build();
+    }
+}
