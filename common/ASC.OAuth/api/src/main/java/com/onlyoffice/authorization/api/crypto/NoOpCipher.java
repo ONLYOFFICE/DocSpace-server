0dd3901cf8d5d395817722e271f97e75ed643fc0
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-10-09 17:20:21 +0500

    fix: profile based cipher

---
+++ common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/crypto/NoOpCipher.java
@@ -0,0 +1,16 @@
+package com.onlyoffice.authorization.api.crypto;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile(value = {"dev", "development", "test", "testing", "d", "t"})
+@Component
+public class NoOpCipher implements Cipher {
+    public String encrypt(String plainMessage) {
+        return plainMessage;
+    }
+
+    public String decrypt(String cipherMessage) {
+        return cipherMessage;
+    }
+}
