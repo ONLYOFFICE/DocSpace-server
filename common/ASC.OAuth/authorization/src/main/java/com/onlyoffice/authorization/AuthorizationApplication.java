8b74368fa9b06f5f55119f17f2f22ad1a32b63c9
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-10-10 20:17:28 +0500

    chore: enable aspectj

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/AuthorizationApplication.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/AuthorizationApplication.java
@@ -2,12 +2,14 @@ package com.onlyoffice.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableFeignClients
+@EnableAspectJAutoProxy
 @SpringBootApplication
 public class AuthorizationApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationApplication.class, args);
 	}
-
 }
