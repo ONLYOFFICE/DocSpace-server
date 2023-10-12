78b81be256deda790ce22a9b68147b1959fd53d4
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-10-12 23:16:52 +0500

    fix: cookie invalidation logic

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/aspect/SessionInvalidationAspect.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/aspect/SessionInvalidationAspect.java
@@ -1,9 +1,11 @@
 package com.onlyoffice.authorization.aspect;
 
+import com.onlyoffice.authorization.configuration.DocspaceConfiguration;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,9 +17,14 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Arrays;
+
 @Component
+@RequiredArgsConstructor
 @Aspect
 public class SessionInvalidationAspect {
+    private final DocspaceConfiguration docspaceConfiguration;
+
     @Value("${server.servlet.session.cookie.name:JSESSIONID}")
     private String JSESSIONID;
     @Around("@annotation(InvalidateSession)")
@@ -29,27 +36,27 @@ public class SessionInvalidationAspect {
         HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder
                 .getRequestAttributes()).getResponse();
 
-        boolean isSecure = false;
-        String contextPath = null;
-
-        if (request != null) {
+        if (request != null && Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equalsIgnoreCase("asc_auth_key"))
+                .findFirst().isEmpty()) {
             HttpSession session = request.getSession(false);
             if (session != null)
                 session.invalidate();
-            isSecure = request.isSecure();
-            contextPath = request.getContextPath();
-        }
+            boolean isSecure = request.isSecure();
+            String contextPath = request.getContextPath();
 
-        SecurityContext context = SecurityContextHolder.getContext();
-        SecurityContextHolder.clearContext();
-        context.setAuthentication(null);
-        if (response != null) {
-            Cookie cookie = new Cookie(JSESSIONID, null);
-            String cookiePath = StringUtils.hasText(contextPath) ? contextPath : "/";
-            cookie.setPath(cookiePath);
-            cookie.setMaxAge(0);
-            cookie.setSecure(isSecure);
-            response.addCookie(cookie);
+            SecurityContext context = SecurityContextHolder.getContext();
+            SecurityContextHolder.clearContext();
+            context.setAuthentication(null);
+            if (response != null) {
+                Cookie cookie = new Cookie(JSESSIONID, null);
+                String cookiePath = StringUtils.hasText(contextPath) ? contextPath : "/";
+                cookie.setPath(cookiePath);
+                cookie.setMaxAge(0);
+                cookie.setSecure(isSecure);
+                response.addCookie(cookie);
+                response.sendRedirect(docspaceConfiguration.getUrl());
+            }
         }
 
         return pjp.proceed();
