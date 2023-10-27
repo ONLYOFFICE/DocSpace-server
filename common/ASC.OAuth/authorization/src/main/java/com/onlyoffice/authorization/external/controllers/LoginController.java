98dd5b313f151b48085c90718470fb42e3939f04
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-10-27 16:58:41 +0500

    fix(authorization): error on first redirect to login

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/external/controllers/LoginController.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/external/controllers/LoginController.java
@@ -8,8 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
 
 /**
@@ -19,13 +19,13 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 @Slf4j
 public class LoginController {
-    private final String CLIENT_ID_COOKIE = "client_id";
+    private final String CLIENT_ID = "client_id";
     private final ClientPersistenceQueryUsecases queryUsecases;
 
     @GetMapping("/oauth2/login")
     public String login(
             HttpServletRequest request,
-            @CookieValue(name = CLIENT_ID_COOKIE) String clientId
+            @RequestParam(name = CLIENT_ID) String clientId
     ) {
         var client = queryUsecases.getClientByClientId(clientId);
         return String.format("redirect:%s", UriComponentsBuilder
