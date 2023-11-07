76d2c2d13443b9f2e877073702c6295f86bb2c4b
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-07 17:19:19 +0500

    chore(api): remove default timezone

--- common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/ApplicationAPI.java
+++ common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/ApplicationAPI.java
@@ -6,11 +6,7 @@ package com.onlyoffice.authorization.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import java.util.TimeZone;
 
 /**
  *
@@ -21,10 +17,5 @@ import java.util.TimeZone;
 public class ApplicationAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationAPI.class, args);
-	}
-
-	@EventListener(ContextRefreshedEvent.class)
-	public void contextRefreshedEvent() {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 }
