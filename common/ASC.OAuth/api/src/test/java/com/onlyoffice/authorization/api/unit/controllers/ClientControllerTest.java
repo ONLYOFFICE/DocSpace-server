0ec34f49455b92d59046991f6a5a1df17115220d
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-08 16:48:06 +0500

    fix(tests): client controller tests

--- common/ASC.OAuth/api/src/test/java/com/onlyoffice/authorization/api/unit/controllers/ClientControllerTest.java
+++ common/ASC.OAuth/api/src/test/java/com/onlyoffice/authorization/api/unit/controllers/ClientControllerTest.java
@@ -8,8 +8,14 @@ import com.onlyoffice.authorization.api.configuration.ApplicationConfiguration;
 import com.onlyoffice.authorization.api.core.transfer.request.CreateClientDTO;
 import com.onlyoffice.authorization.api.core.transfer.response.ClientDTO;
 import com.onlyoffice.authorization.api.core.transfer.response.PaginationDTO;
+import com.onlyoffice.authorization.api.core.transfer.response.docspace.DocspaceResponseDTO;
+import com.onlyoffice.authorization.api.core.transfer.response.docspace.MeDTO;
+import com.onlyoffice.authorization.api.core.transfer.response.docspace.TenantDTO;
+import com.onlyoffice.authorization.api.external.clients.DocspaceClient;
 import com.onlyoffice.authorization.api.external.controllers.ClientController;
 import com.onlyoffice.authorization.api.ports.services.ClientService;
+import com.onlyoffice.authorization.api.security.filters.CheckAuthAdminCookieFilter;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -24,6 +30,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Set;
 
@@ -39,6 +46,8 @@ public class ClientControllerTest {
     @Mock
     private ClientService clientService;
     @Mock
+    private DocspaceClient docspaceClient;
+    @Mock
     private ApplicationConfiguration applicationConfiguration;
     @InjectMocks
     private ClientController clientController;
@@ -49,22 +58,70 @@ public class ClientControllerTest {
     void setup() {
         JacksonTester.initFields(this, new ObjectMapper());
         mvc = MockMvcBuilders.standaloneSetup(clientController)
+                .addFilter(new CheckAuthAdminCookieFilter(docspaceClient))
                 .build();
     }
 
     @Test
     void canRetrieve() throws Exception {
+        given(docspaceClient.getMe(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<MeDTO>builder()
+                        .statusCode(200)
+                        .statusCode(200)
+                        .response(MeDTO
+                                .builder()
+                                .userName("Admin")
+                                .firstName("Admin")
+                                .lastName("Admin")
+                                .email("admin@admin.com")
+                                .avatarSmall("smallAvatar")
+                                .avatar("avatar")
+                                .isAdmin(true)
+                                .build())
+                        .build());
+        given(docspaceClient.getProfile(URI.create("http://127.0.0.1"), "asc_auth_key=zxc", "admin@admin.com"))
+                .willReturn(DocspaceResponseDTO
+                        .<MeDTO>builder()
+                        .statusCode(200)
+                        .statusCode(200)
+                        .response(MeDTO
+                                .builder()
+                                .userName("Admin")
+                                .firstName("Admin")
+                                .lastName("Admin")
+                                .email("admin@admin.com")
+                                .avatarSmall("smallAvatar")
+                                .avatar("avatar")
+                                .build())
+                        .build());
+        given(docspaceClient.getTenant(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<TenantDTO>builder()
+                        .status(200)
+                        .statusCode(200)
+                        .response(TenantDTO
+                                .builder()
+                                .tenantId(1)
+                                .tenantAlias("tenant")
+                                .name("tenant")
+                                .build())
+                        .build());
         given(clientService.getTenantClients(1, 0, 5))
                 .willReturn(PaginationDTO.builder()
-                        .data(List.of(ClientDTO.builder().clientId("mock").tenant(1).build()))
+                        .data(List.of(ClientDTO.builder().modifiedBy("admin@admin.com")
+                                .clientId("mock").tenant(1).build()))
                         .limit(5)
                         .page(0)
                         .build());
         MockHttpServletResponse response = mvc.perform(
                         MockMvcRequestBuilders.get("/api/2.0/clients")
-                                .header("X-Tenant", "1")
                                 .queryParam("page", "0")
                                 .queryParam("limit", "5")
+                                .cookie(new Cookie[]{
+                                        new Cookie("asc_auth_key", "zxc"),
+                                        new Cookie("x-docspace-address", "http://127.0.0.1")
+                                })
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
@@ -72,11 +129,42 @@ public class ClientControllerTest {
 
     @Test
     void canRetrieveByIdAndTenant() throws Exception {
+        given(docspaceClient.getMe(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<MeDTO>builder()
+                        .statusCode(200)
+                        .statusCode(200)
+                        .response(MeDTO
+                                .builder()
+                                .userName("Admin")
+                                .firstName("Admin")
+                                .lastName("Admin")
+                                .email("admin@admin.com")
+                                .avatarSmall("smallAvatar")
+                                .avatar("avatar")
+                                .isAdmin(true)
+                                .build())
+                        .build());
+        given(docspaceClient.getTenant(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<TenantDTO>builder()
+                        .status(200)
+                        .statusCode(200)
+                        .response(TenantDTO
+                                .builder()
+                                .tenantId(1)
+                                .tenantAlias("tenant")
+                                .name("tenant")
+                                .build())
+                        .build());
         given(clientService.getClient("mock"))
                 .willReturn(ClientDTO.builder().clientId("mock").tenant(1).build());
         MockHttpServletResponse response = mvc.perform(
                         MockMvcRequestBuilders.get("/api/2.0/clients/mock")
-                                .header("X-Tenant", "1")
+                                .cookie(new Cookie[]{
+                                        new Cookie("asc_auth_key", "zxc"),
+                                        new Cookie("x-docspace-address", "http://127.0.0.1")
+                                })
                                 .accept(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
@@ -84,16 +172,34 @@ public class ClientControllerTest {
 
     @Test
     void canCreateClient() throws Exception {
-        given(clientService.clientAsyncCreationTask(CreateClientDTO
-                .builder()
-                        .name("mock")
-                .build(), 1, "http://127.0.0.1")).willReturn(ClientDTO
-                .builder()
-                        .name("mock")
-                .build());
-        given(applicationConfiguration.getScopes())
-                .willReturn(List.of(new ApplicationConfiguration
-                        .ScopeConfiguration("mock", "mock", "mock")));
+        given(docspaceClient.getMe(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<MeDTO>builder()
+                        .statusCode(200)
+                        .statusCode(200)
+                        .response(MeDTO
+                                .builder()
+                                .userName("Admin")
+                                .firstName("Admin")
+                                .lastName("Admin")
+                                .email("admin@admin.com")
+                                .avatarSmall("smallAvatar")
+                                .avatar("avatar")
+                                .isAdmin(true)
+                                .build())
+                        .build());
+        given(docspaceClient.getTenant(URI.create("http://127.0.0.1"), "asc_auth_key=zxc"))
+                .willReturn(DocspaceResponseDTO
+                        .<TenantDTO>builder()
+                        .status(200)
+                        .statusCode(200)
+                        .response(TenantDTO
+                                .builder()
+                                .tenantId(1)
+                                .tenantAlias("tenant")
+                                .name("tenant")
+                                .build())
+                        .build());
         MockHttpServletResponse response = mvc.perform(
                         MockMvcRequestBuilders.post("/api/2.0/clients")
                                 .contentType(MediaType.APPLICATION_JSON)
@@ -104,15 +210,20 @@ public class ClientControllerTest {
                                                 .description("mockmockmockmockmockmockmockmockmockmock")
                                                 .termsUrl("https://example.com")
                                                 .policyUrl("https://example.com")
-                                                .logo("https://example.com")
+                                                .logo("data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48IS0tIFVwbG9hZGVkIHRvOiBTVkcgUmVwbywgd3d3LnN2Z3JlcG8uY29tLCBHZW5lcmF0b3I6IFNWRyBSZXBvIE1peGVyIFRvb2xzIC0tPg0KPHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik03IDEySDE3TTggOC41QzggOC41IDkgOSAxMCA5QzExLjUgOSAxMi41IDggMTQgOEMxNSA4IDE2IDguNSAxNiA4LjVNOCAxNS41QzggMTUuNSA5IDE2IDEwIDE2QzExLjUgMTYgMTIuNSAxNSAxNCAxNUMxNSAxNSAxNiAxNS41IDE2IDE1LjVNMjEgMTJDMjEgMTYuOTcwNiAxNi45NzA2IDIxIDEyIDIxQzcuMDI5NDQgMjEgMyAxNi45NzA2IDMgMTJDMyA3LjAyOTQ0IDcuMDI5NDQgMyAxMiAzQzE2Ljk3MDYgMyAyMSA3LjAyOTQ0IDIxIDEyWiIgc3Ryb2tlPSIjMDAwMDAwIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPg0KPC9zdmc+")
+                                                .websiteUrl("https://example.com")
                                                 .authenticationMethod("mock")
                                                 .redirectUris(Set.of("https://example.com"))
                                                 .logoutRedirectUri("https://example.com")
-                                                .scopes(Set.of("mock"))
+                                                .allowedOrigins(Set.of("https://example.com"))
+                                                .scopes(Set.of("accounts:read"))
                                                 .build()).
                                         getJson())
-                                .header("X-Tenant", "1")
+                                .cookie(new Cookie[]{
+                                        new Cookie("asc_auth_key", "zxc"),
+                                        new Cookie("x-docspace-address", "http://127.0.0.1")
+                                })
                 ).andReturn().getResponse();
-        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
