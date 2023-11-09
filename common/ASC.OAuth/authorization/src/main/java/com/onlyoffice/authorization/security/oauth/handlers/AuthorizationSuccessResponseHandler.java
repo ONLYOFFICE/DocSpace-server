f10977b5819d9936ad90dd6d78c1080d4069523b
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-09 16:03:52 +0500

    fix(authorization): set x-redirect header instead of actual redirect

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/security/oauth/handlers/AuthorizationSuccessResponseHandler.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/security/oauth/handlers/AuthorizationSuccessResponseHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationToken;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -14,12 +15,14 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class AuthorizationSuccessResponseHandler implements AuthenticationSuccessHandler {
+    private final String REDIRECT_HEADER = "X-Redirect-URI";
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("Authorization success");
         if (authentication instanceof OAuth2AuthorizationCodeRequestAuthenticationToken token) {
             log.info("Redirecting to redirect uri with authorization code");
-            response.sendRedirect(String
+            response.setStatus(HttpStatus.OK.value());
+            response.setHeader(REDIRECT_HEADER, String
                     .format("%s?code=%s", token.getRedirectUri(),
                             token.getAuthorizationCode().getTokenValue()));
         }
