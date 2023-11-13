e6f1935b06fb945d15e3b8f21d019cf530bcac27
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-13 18:28:50 +0500

    chore(authorization): undo remove cookie

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/security/oauth/handlers/AuthorizationSuccessResponseHandler.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/security/oauth/handlers/AuthorizationSuccessResponseHandler.java
@@ -1,7 +1,6 @@
 package com.onlyoffice.authorization.security.oauth.handlers;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -16,16 +15,12 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class AuthorizationSuccessResponseHandler implements AuthenticationSuccessHandler {
-    private final String CLIENT_STATE_COOKIE = "client_state";
     private final String REDIRECT_HEADER = "X-Redirect-URI";
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("Authorization success");
         if (authentication instanceof OAuth2AuthorizationCodeRequestAuthenticationToken token) {
             log.info("Redirecting to redirect uri with authorization code");
-            Cookie cookie = new Cookie(CLIENT_STATE_COOKIE, "");
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
             response.setStatus(HttpStatus.OK.value());
             response.setHeader(REDIRECT_HEADER, String
                     .format("%s?code=%s", token.getRedirectUri(),
