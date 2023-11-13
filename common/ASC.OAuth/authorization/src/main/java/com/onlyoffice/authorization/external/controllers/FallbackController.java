ecdede142a4bb66f22bdc840d56d632fb90fb663
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-13 20:42:08 +0500

    fix(authorization): redirect to docspace on error

--- common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/external/controllers/FallbackController.java
+++ common/ASC.OAuth/authorization/src/main/java/com/onlyoffice/authorization/external/controllers/FallbackController.java
@@ -3,20 +3,31 @@
  */
 package com.onlyoffice.authorization.external.controllers;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 /**
  *
  */
 @Controller
-public class FallbackController implements ErrorController {
+public class FallbackController extends AbstractErrorController {
     private static final String PATH = "/error";
 
-    @RequestMapping(value = PATH)
-    public String error() {
-        return "error";
+    public FallbackController(ErrorAttributes errorAttributes) {
+        super(errorAttributes);
+    }
+
+    @RequestMapping(PATH)
+    public void handleError(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        response.sendRedirect(String.format("%s://%s/login/?type=oauth2&clientId=error",
+                request.getScheme(), request.getRemoteHost()));
     }
 
     public String getErrorPath() {
