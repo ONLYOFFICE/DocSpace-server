08bbc71ee8ea8cb118c35aa07866b985e1ae4a68
Andrey Savihin <andrey.savihin@onlyoffice.com>
2023-11-14 17:44:24 +0300

    fix Bug 64787

--- products/ASC.Files/Core/Helpers/CspSettingsHelper.cs
+++ products/ASC.Files/Core/Helpers/CspSettingsHelper.cs
@@ -210,7 +210,7 @@ public class CspSettingsHelper
             }
         }
 
-        if (!string.IsNullOrEmpty(_configuration["files:oform:url"]))
+        if (!string.IsNullOrEmpty(_configuration["files:oform:domain"]))
         {
             var oformOptions = _configuration.GetSection("csp:oform").Get<CspOptions>();
             if (oformOptions != null)
