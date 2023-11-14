c0646be6f8fc502a4998c6ae8485f28dbb945817
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-11-14 16:02:49 +0500

    fix(api): consent performance tweak

--- common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/core/usecases/repository/consent/ConsentPersistenceRetrieveUsecases.java
+++ common/ASC.OAuth/api/src/main/java/com/onlyoffice/authorization/api/core/usecases/repository/consent/ConsentPersistenceRetrieveUsecases.java
@@ -1,9 +1,12 @@
 package com.onlyoffice.authorization.api.core.usecases.repository.consent;
 
 import com.onlyoffice.authorization.api.core.entities.Consent;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
 public interface ConsentPersistenceRetrieveUsecases {
-    Set<Consent> findAllByPrincipalName(String principalName);
+    @Query("SELECT c, cl FROM Consent c INNER JOIN c.client cl WHERE c.registeredClientId = cl.clientId AND c.principalName = :principalName")
+    Set<Consent> findAllByPrincipalName(@Param("principalName") String principalName);
 }
