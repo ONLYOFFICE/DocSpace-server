5dbccca29b9f70d0492482841597f526fe645ba4
pavelbannov <pavel.bannov@onlyoffice.com>
2023-12-12 19:59:48 +0300

    fix Bug 65479

--- web/ASC.Web.Core/Notify/StudioWhatsNewNotify.cs
+++ web/ASC.Web.Core/Notify/StudioWhatsNewNotify.cs
@@ -365,7 +365,12 @@ public class StudioWhatsNewNotify
             d = d.AddHours(-1);
         }
 
-        return d.ToString(c.TwoLetterISOLanguageName == "ru" ? "d MMMM" : "M", c);
+        if (c.TwoLetterISOLanguageName == "ru")
+        {
+            return d.ToString("d MMMM", c);
+        }
+        
+        return d.ConvertNumerals("M");
     }
 }
 
