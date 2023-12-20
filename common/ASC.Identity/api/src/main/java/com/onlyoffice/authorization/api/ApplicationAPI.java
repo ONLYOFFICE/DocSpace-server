291357ceac4ad7dee6991b1f843f16742e19e8d8
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2023-12-20 13:12:29 +0300

    fix(api): exclude userdetails service autoconfiguration

--- common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/ApplicationAPI.java
+++ common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/ApplicationAPI.java
@@ -5,6 +5,7 @@ package com.onlyoffice.authorization.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,7 +18,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableFeignClients
 @EnableTransactionManagement
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class ApplicationAPI {
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationAPI.class, args);
