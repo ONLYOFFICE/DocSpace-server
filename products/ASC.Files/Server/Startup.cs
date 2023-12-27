0e28f76165e080216414e5f3a4fafd7fd27cdf74
Alexey Bannov <alexey.bannov@onlyoffice.com>
2023-12-27 13:04:05 +0300

    rabbitmq: setup clientname

--- products/ASC.Files/Server/Startup.cs
+++ products/ASC.Files/Server/Startup.cs
@@ -34,6 +34,11 @@ public class Startup : BaseStartup
         : base(configuration, hostEnvironment)
     {
         WebhooksEnabled = true;
+
+        if (String.IsNullOrEmpty(configuration["RabbitMQ:ClientProvidedName"]))
+        {
+            configuration["RabbitMQ:ClientProvidedName"] = Program.AppName;
+        }
     }
 
     public override void ConfigureServices(IServiceCollection services)
