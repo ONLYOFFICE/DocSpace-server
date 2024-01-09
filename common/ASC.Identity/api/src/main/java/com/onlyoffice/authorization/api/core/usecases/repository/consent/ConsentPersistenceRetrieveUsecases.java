44a1a38c1f3447e9dec3c0f4778ad9ddd67796e1
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-01-09 20:08:34 +0500

    chore(api): decouple consent retrieve usecases from repository

--- common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/core/usecases/repository/consent/ConsentPersistenceRetrieveUsecases.java
+++ common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/core/usecases/repository/consent/ConsentPersistenceRetrieveUsecases.java
@@ -1,8 +1,6 @@
 package com.onlyoffice.authorization.api.core.usecases.repository.consent;
 
 import com.onlyoffice.authorization.api.core.entities.Consent;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
@@ -15,6 +13,5 @@ public interface ConsentPersistenceRetrieveUsecases {
      * @param principalName
      * @return
      */
-    @Query("SELECT c, cl FROM Consent c INNER JOIN c.client cl WHERE c.registeredClientId = cl.clientId AND c.principalName = :principalName AND cl.invalidated = false")
-    Set<Consent> findAllByPrincipalName(@Param("principalName") String principalName);
+    Set<Consent> findAllByPrincipalName(String principalName);
 }
--- common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/web/server/ports/repositories/ConsentRepository.java
+++ common/ASC.Identity/api/src/main/java/com/onlyoffice/authorization/api/web/server/ports/repositories/ConsentRepository.java
@@ -7,7 +7,11 @@ import com.onlyoffice.authorization.api.core.entities.Consent;
 import com.onlyoffice.authorization.api.core.usecases.repository.consent.ConsentPersistenceCleanupUsecases;
 import com.onlyoffice.authorization.api.core.usecases.repository.consent.ConsentPersistenceCreationUsecases;
 import com.onlyoffice.authorization.api.core.usecases.repository.consent.ConsentPersistenceRetrieveUsecases;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Set;
 
 /**
  *
@@ -23,10 +27,27 @@ public interface ConsentRepository extends CrudRepository<Consent, Consent.Conse
 
     /**
      *
+     * @param principalName
+     * @return
+     */
+    @Query("SELECT c, cl FROM Consent c INNER JOIN c.client cl WHERE c.registeredClientId = cl.clientId AND c.principalName = :principalName AND cl.invalidated = false")
+    Set<Consent> findAllConsentsByPrincipalName(@Param("principalName") String principalName);
+
+    /**
+     *
      * @param entity
      * @return
      */
     default Consent saveConsent(Consent entity) {
         return this.save(entity);
+    }
+
+    /**
+     *
+     * @param principalName
+     * @return
+     */
+    default Set<Consent> findAllByPrincipalName(String principalName) {
+        return findAllConsentsByPrincipalName(principalName);
     }
 }
