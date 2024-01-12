2edb69e6df28d2f6862549693ec3e0e596ab5fac
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-01-12 21:01:59 +0500

    chore(authorization)!: remove get client call from login controller

--- common/ASC.Identity/authorization/src/main/java/com/onlyoffice/authorization/web/server/controllers/LoginController.java
+++ common/ASC.Identity/authorization/src/main/java/com/onlyoffice/authorization/web/server/controllers/LoginController.java
@@ -3,7 +3,8 @@
  */
 package com.onlyoffice.authorization.web.server.controllers;
 
-import com.onlyoffice.authorization.core.usecases.repositories.ClientPersistenceQueryUsecases;
+import com.onlyoffice.authorization.web.server.utilities.HttpUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
@@ -20,7 +21,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 @RequiredArgsConstructor
 public class LoginController {
     private final String CLIENT_ID = "client_id";
-    private final ClientPersistenceQueryUsecases queryUsecases;
 
     /**
      *
@@ -28,14 +28,16 @@ public class LoginController {
      * @return
      */
     @GetMapping("/oauth2/login")
-    public String login(@RequestParam(name = CLIENT_ID) String clientId) {
+    public String login(
+            HttpServletRequest request,
+            @RequestParam(name = CLIENT_ID) String clientId
+    ) {
         MDC.put("client_id", clientId);
         log.info("Get login request");
         MDC.clear();
 
-        var client = queryUsecases.getClientByClientId(clientId);
         return String.format("redirect:%s", UriComponentsBuilder
-                .fromUriString(client.getTenantUrl())
+                .fromUriString(HttpUtils.getRequestIP(request))
                 .path("login")
                 .queryParam("client_id", clientId)
                 .queryParam("type", "oauth2")
