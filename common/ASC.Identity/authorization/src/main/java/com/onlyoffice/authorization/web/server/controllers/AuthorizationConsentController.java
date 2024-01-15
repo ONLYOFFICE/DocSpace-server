430c5f19ee47d49cd16fb29a80618febb9f15959
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-01-15 11:07:57 +0300

    fix(authorization)!: consent page redirect

--- common/ASC.Identity/authorization/src/main/java/com/onlyoffice/authorization/web/server/controllers/AuthorizationConsentController.java
+++ common/ASC.Identity/authorization/src/main/java/com/onlyoffice/authorization/web/server/controllers/AuthorizationConsentController.java
@@ -3,19 +3,15 @@
  */
 package com.onlyoffice.authorization.web.server.controllers;
 
-import com.onlyoffice.authorization.core.usecases.repositories.AuthorizationPersistenceQueryUsecases;
-import com.onlyoffice.authorization.core.usecases.repositories.ClientPersistenceQueryUsecases;
-import com.onlyoffice.authorization.web.clients.APIClient;
+import com.onlyoffice.authorization.web.server.utilities.HttpUtils;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 /**
  *
@@ -24,52 +20,21 @@ import java.net.URI;
 @Controller
 @RequiredArgsConstructor
 public class AuthorizationConsentController {
-    private final String ASC_AUTH_COOKIE = "asc_auth_key";
     private final String CLIENT_ID = "client_id";
-
-    private final APIClient docspaceClient;
-    private final AuthorizationPersistenceQueryUsecases authorizationRepository;
-    private final ClientPersistenceQueryUsecases queryUsecases;
 
     @GetMapping(value = "/oauth2/consent")
     public String consent(
-            @CookieValue(name = ASC_AUTH_COOKIE) String authCookie,
-            @RequestParam(name = CLIENT_ID) String clientId
+            HttpServletRequest request,
+            @RequestParam(CLIENT_ID) String clientId
     ) {
         MDC.put("clientId", clientId);
         log.info("Got a new consent request");
-        log.debug("Trying to get client by client id");
 
-        var client = queryUsecases.getClientByClientId(clientId);
-        try {
-            var cookie = String.format("%s=%s", "asc_auth_key", authCookie);
-            var me = docspaceClient.getMe(URI.create(client.getTenantUrl()), cookie)
-                    .getResponse();
-
-            MDC.put("clientId", clientId);
-            MDC.put("principalName", me.getEmail());
-            log.info("Trying to get consent by principal name and client");
-
-            var auth = authorizationRepository.getByPrincipalNameAndRegisteredClientId(me.getEmail(), clientId);
-            return String.format("redirect:%s", UriComponentsBuilder
-                    .fromUriString(client.getTenantUrl())
-                    .path("login/consent")
-                    .queryParam("type", "oauth2")
-                    .queryParam("client_id", clientId)
-                    .queryParam("state", auth.getState())
-                    .build());
-        } catch (Exception e) {
-            MDC.put("message", e.getMessage());
-            log.info("Could not redirect to consent page. Redirecting to login");
-
-            return String.format("redirect:%s", UriComponentsBuilder
-                    .fromUriString(client.getTenantUrl())
-                    .path("login")
-                    .queryParam("type", "oauth2")
-                    .queryParam("client_id", clientId)
-                    .build());
-        } finally {
-            MDC.clear();
-        }
+        return String.format("redirect:%s", UriComponentsBuilder
+                .fromUriString(HttpUtils.getRequestIP(request))
+                .path("login")
+                .queryParam("type", "oauth2")
+                .queryParam("client_id", clientId)
+                .build());
     }
 }
