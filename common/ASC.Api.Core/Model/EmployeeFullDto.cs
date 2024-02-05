ba636b289838d0515408d09f1a9f43c8c1a01275
Nikolay Rechkin <nikolay.rechkin@onlyoffice.com>
2024-02-05 15:19:30 +0300

    Quota: return of own quota

--- common/ASC.Api.Core/Model/EmployeeFullDto.cs
+++ common/ASC.Api.Core/Model/EmployeeFullDto.cs
@@ -299,7 +299,7 @@ public class EmployeeFullDtoHelper(
 
         await InitAsync(result, userInfo);
 
-        if ((coreBaseSettings.Standalone || (await tenantManager.GetCurrentTenantQuotaAsync()).Statistic) && (await _userManager.IsDocSpaceAdminAsync(_authContext.CurrentAccount.ID)))
+        if ((coreBaseSettings.Standalone || (await tenantManager.GetCurrentTenantQuotaAsync()).Statistic) && (await _userManager.IsDocSpaceAdminAsync(_authContext.CurrentAccount.ID) || userInfo.Id == _authContext.CurrentAccount.ID))
         {
             var quotaSettings = await settingsManager.LoadAsync<TenantUserQuotaSettings>();
             result.UsedSpace = Math.Max(0, (await quotaService.FindUserQuotaRowsAsync(tenant.Id, userInfo.Id)).Where(r => !string.IsNullOrEmpty(r.Tag)).Sum(r => r.Counter));
