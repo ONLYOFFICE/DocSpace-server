acf93cc4f82b590047d1af1ae293d9caa4397040
Andrey Savihin <andrey.savihin@onlyoffice.com>
2024-02-27 18:19:43 +0300

    ASC.Web.Core: NotifyTags: button styles changed

--- common/ASC.ActiveDirectory/Base/NotifyConstants.cs
+++ common/ASC.ActiveDirectory/Base/NotifyConstants.cs
@@ -36,12 +36,33 @@ public static class NotifyConstants
 
     public static ITagValue TagOrangeButton(string btnText, string btnUrl)
     {
-        const string td = "<td style=\"height: 48px; width: 80px; margin:0; padding-bottom:56px;\">&nbsp;</td>";
-        const string color = "background-color:#FF6F3D; border:1px solid #FF6F3D; border-radius: 3px; color:#ffffff; display: inline-block; font-family: 'Open Sans', Helvetica, Arial, Tahoma, sans-serif; font-size: 13px; font-weight: 600; padding-top: 15px; padding-right: 25px; padding-bottom: 15px; padding-left: 25px; text-align: center; text-decoration: none; text-transform: uppercase; -webkit-text-size-adjust: none; letter-spacing: 0.04em;";
+        var value = $@"<table cellspacing=""0"" cellpadding=""0"" style=""border: 0 none; border-collapse: collapse; border-spacing: 0; empty-cells: show; margin: 0 auto; max-width: 600px; padding: 0; vertical-align: top; width: 100%; text-align: left;"">
+            <tbody>
+                <tr border=""0"" cellspacing=""0"" cellpadding=""0"">
+                    <td style=""width: 180px;""></td>
+                    <!--[if mso]>
+                        <td class=""body-text"" border=""0"" style=""margin: 0; padding: 0; text-align: center; width: 230px;"">
+                            <v:roundrect xmlns:v=""urn:schemas-microsoft-com:vml"" xmlns:w=""urn:schemas-microsoft-com:office:word"" href=""{btnUrl}"" style=""v-text-anchor: middle; box-sizing: border-box; font-weight: 600; font-size: 12px; height: 56px; text-decoration: none; width: 230px;"" arcsize=""5%"" strokecolor=""#FF6F3D"" fillcolor=""#FF6F3D"" alt=""{btnText}"" target=""_blank"">
+                                <w:anchorlock/>
+                            <center class=""fol"" style=""color:#ffffff; font-family: 'Open Sans', Helvetica, Arial, Tahoma, sans-serif; font-weight: 600; font-size: 12px; letter-spacing: 0.04em; text-align: center; text-decoration: none; text-transform: uppercase; white-space: nowrap;"">
+                               {btnText}
+                            </center>
+                            </v:roundrect>
+                        </td>
+                    <![endif]-->
+                    <!--[if !mso]> <!-->
+                        <td>
+                            <a class=""fol"" href=""{btnUrl}"" style=""background-color:#FF6F3D; border:1px solid #FF6F3D; border-radius: 3px; color:#ffffff; display: inline-block; font-family: 'Open Sans', Helvetica, Arial, Tahoma, sans-serif; font-size: 12px; font-weight: 600; padding-top: 15px; padding-right: 83px; padding-bottom: 15px; padding-left: 83px; text-align: center; text-decoration: none; text-transform: uppercase; -webkit-text-size-adjust: none; mso-hide: all; white-space: nowrap; letter-spacing: 0.04em;"" alt=""{btnText}"" target=""_blank"">
+                                {btnText}
+                            </a>
+                        </td>
+                    <!-- <![endif]-->
+                    <td style=""width: 180px;""></td>
+                 </tr>
+            </tbody>
+        </table>";
 
-        var action = $@"<table style=""border: 0 none; border-collapse: collapse; border-spacing: 0; empty-cells: show; margin: 0 auto; max-width: 600px; padding: 0; vertical-align: top; width: 100%; text-align: left;""><tbody><tr cellpadding=""0"" cellspacing=""0"" border=""0"">{td}<td style=""height: 48px; width: 380px; margin:0; padding:0; text-align:center;""><a style=""{color}"" target=""_blank"" href=""{btnUrl}"">{btnText}</a></td>{td}</tr></tbody></table>";
-
-        return new TagValue("OrangeButton", action);
+        return new TagValue("OrangeButton", value);
     }
 }
 
