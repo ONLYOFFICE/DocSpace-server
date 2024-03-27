4e9b7a8230e861adc5c7f60e55dc8f9937696089
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2024-03-27 12:42:00 +0300

    Bug 67078

--- products/ASC.Files/Core/Core/Security/FileSecurity.cs
+++ products/ASC.Files/Core/Core/Security/FileSecurity.cs
@@ -1059,12 +1059,6 @@ public class FileSecurity(IDaoFactory daoFactory,
             }
         }
 
-        if (ace is { SubjectType: SubjectType.ExternalLink or SubjectType.PrimaryExternalLink } && ace.Subject != userId && 
-            await externalShare.ValidateRecordAsync(ace, null, isAuthenticated, e) != Status.Ok)
-        {
-            return false;
-        }
-
         var defaultShare =
             e.RootFolderType == FolderType.VirtualRooms ? DefaultVirtualRoomsShare :
             e.RootFolderType == FolderType.USER ? DefaultMyShare :
@@ -1075,6 +1069,12 @@ public class FileSecurity(IDaoFactory daoFactory,
         e.ShareRecord = ace;
         e.Access = ace?.Share ?? defaultShare;
         e.Access = e.RootFolderType is FolderType.ThirdpartyBackup ? FileShare.Restrict : e.Access;
+        
+        if (ace is { SubjectType: SubjectType.ExternalLink or SubjectType.PrimaryExternalLink } && ace.Subject != userId && 
+            await externalShare.ValidateRecordAsync(ace, null, isAuthenticated, e) != Status.Ok)
+        {
+            return false;
+        }
 
         switch (action)
         {
