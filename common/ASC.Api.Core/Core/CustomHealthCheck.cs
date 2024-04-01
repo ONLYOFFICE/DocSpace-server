0ef4602cd4c7172719ba2f0d477db96780f10c22
pavelbannov <pavel.bannov@onlyoffice.com>
2024-04-01 17:25:10 +0300

    Remove Elasticsearch health check
    The Elasticsearch package reference and its associated health check method have been removed from the ASC.Api.Core project.

--- common/ASC.Api.Core/Core/CustomHealthCheck.cs
+++ common/ASC.Api.Core/Core/CustomHealthCheck.cs
@@ -43,8 +43,7 @@ public static class CustomHealthCheck
                                     : HealthCheckResult.Unhealthy())
                  .AddDatabase(configuration)
                  .AddDistibutedCache(configuration)
-                 .AddMessageQueue(configuration)
-                 .AddSearch(configuration);
+                 .AddMessageQueue(configuration);
 
         return services;
     }
@@ -60,32 +59,6 @@ public static class CustomHealthCheck
                                name: "redis",
                                tags: new[] { "redis", "services" },
                                timeout: new TimeSpan(0, 0, 15));
-        }
-
-        return hcBuilder;
-    }
-
-
-
-    public static IHealthChecksBuilder AddSearch(
-   this IHealthChecksBuilder hcBuilder, IConfiguration configuration)
-    {
-        var elasticSettings = configuration.GetSection("elastic");
-
-        if (elasticSettings.GetChildren().Any())
-        {
-            var host = elasticSettings.GetSection("Host").Value ?? "localhost";
-            var scheme = elasticSettings.GetSection("Scheme").Value ?? "http";
-            var port = elasticSettings.GetSection("Port").Value ?? "9200";
-            var elasticSearchUri = $"{scheme}://{host}:{port}";
-
-            if (Uri.IsWellFormedUriString(elasticSearchUri, UriKind.Absolute))
-            {
-                hcBuilder.AddElasticsearch(elasticSearchUri,
-                                          name: "elasticsearch",
-                                          tags: new[] { "elasticsearch", "services" },
-                                          timeout: new TimeSpan(0, 0, 15));
-            }
         }
 
         return hcBuilder;
