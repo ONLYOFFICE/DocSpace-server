119df4694614dd827091045ea850b515d1056e34
Maria-Sukhova <Maria.Sukhova@onlyoffice.com>
2024-04-18 16:03:14 +0300

    added sr-latn-RS

--- web/ASC.Web.Core/PublicResources/CustomModeResource.Designer.cs
+++ web/ASC.Web.Core/PublicResources/CustomModeResource.Designer.cs
@@ -8,13 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System.CodeDom.Compiler;
-using System.Diagnostics.CodeAnalysis;
-using System.Resources;
-using System.Runtime.CompilerServices;
-
-namespace ASC.Web.Core.PublicResources
-{
+namespace ASC.Web.Core.PublicResources {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,55 +19,47 @@ namespace ASC.Web.Core.PublicResources
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
-    [DebuggerNonUserCode()]
-    [CompilerGenerated()]
-    public class CustomModeResource
-    {
-
-        private static ResourceManager resourceMan;
-
-        private static CultureInfo resourceCulture;
-
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal CustomModeResource()
-        {
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    public class CustomModeResource {
+        
+        private static global::System.Resources.ResourceManager resourceMan;
+        
+        private static global::System.Globalization.CultureInfo resourceCulture;
+        
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        internal CustomModeResource() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
-        public static ResourceManager ResourceManager
-        {
-            get
-            {
-                if (ReferenceEquals(resourceMan, null))
-                {
-                    ResourceManager temp = new ResourceManager("ASC.Web.Core.PublicResources.CustomModeResource", typeof(CustomModeResource).Assembly);
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ASC.Web.Core.PublicResources.CustomModeResource", typeof(CustomModeResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
-        public static CultureInfo Culture
-        {
-            get
-            {
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        public static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Hello, $UserName!
         ///
@@ -82,14 +71,12 @@ namespace ASC.Web.Core.PublicResources
         ///
         ///* *Collaborate on documents* with two co-editing mo [rest of string was truncated]&quot;;.
         /// </summary>
-        public static string pattern_enterprise_whitelabel_user_welcome_custom_mode_v1
-        {
-            get
-            {
+        public static string pattern_enterprise_whitelabel_user_welcome_custom_mode_v1 {
+            get {
                 return ResourceManager.GetString("pattern_enterprise_whitelabel_user_welcome_custom_mode_v1", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Dear $UserName,
         ///
@@ -97,14 +84,12 @@ namespace ASC.Web.Core.PublicResources
         ///
         ///The deletion of personal data allowed to free $DocsSpace.
         /// </summary>
-        public static string pattern_remove_user_data_completed_custom_mode
-        {
-            get
-            {
+        public static string pattern_remove_user_data_completed_custom_mode {
+            get {
                 return ResourceManager.GetString("pattern_remove_user_data_completed_custom_mode", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to h3.New portal has been registered
         ///
@@ -115,54 +100,44 @@ namespace ASC.Web.Core.PublicResources
         ///# Phone: $Phone
         ///# Creation date: $Date.
         /// </summary>
-        public static string pattern_saas_custom_mode_reg_data
-        {
-            get
-            {
+        public static string pattern_saas_custom_mode_reg_data {
+            get {
                 return ResourceManager.GetString("pattern_saas_custom_mode_reg_data", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Do the same as a user|Link Dropbox, Box and other accounts in the &apos;Common&apos; section|Set up access rights to the documents and folders in the &apos;Common&apos; section.
         /// </summary>
-        public static string ProductAdminOpportunitiesCustomMode
-        {
-            get
-            {
+        public static string ProductAdminOpportunitiesCustomMode {
+            get {
                 return ResourceManager.GetString("ProductAdminOpportunitiesCustomMode", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Create and edit own documents as well as the shared documents with the proper access rights|Give reading/editing access to other users to the documents and folders|Link Dropbox, Box and other accounts in the &apos;My documents&apos; section.
         /// </summary>
-        public static string ProductUserOpportunitiesCustomMode
-        {
-            get
-            {
+        public static string ProductUserOpportunitiesCustomMode {
+            get {
                 return ResourceManager.GetString("ProductUserOpportunitiesCustomMode", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to ${LetterLogoText}. Remove user data is completed.
         /// </summary>
-        public static string subject_remove_user_data_completed_custom_mode
-        {
-            get
-            {
+        public static string subject_remove_user_data_completed_custom_mode {
+            get {
                 return ResourceManager.GetString("subject_remove_user_data_completed_custom_mode", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to New portal has been registered.
         /// </summary>
-        public static string subject_saas_custom_mode_reg_data
-        {
-            get
-            {
+        public static string subject_saas_custom_mode_reg_data {
+            get {
                 return ResourceManager.GetString("subject_saas_custom_mode_reg_data", resourceCulture);
             }
         }
