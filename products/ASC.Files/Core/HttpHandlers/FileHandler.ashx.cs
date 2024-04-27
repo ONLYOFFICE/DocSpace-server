58f6c9d2d53d55a5a60695d01b2c0155d82d2f12
pavelbannov <pavel.bannov@onlyoffice.com>
2024-04-27 17:07:30 +0300

    fix Bug 67662

--- products/ASC.Files/Core/HttpHandlers/FileHandler.ashx.cs
+++ products/ASC.Files/Core/HttpHandlers/FileHandler.ashx.cs
@@ -75,7 +75,8 @@ public class FileHandlerService(FilesLinkUtility filesLinkUtility,
     ThumbnailSettings thumbnailSettings,
     ExternalLinkHelper externalLinkHelper,
     ExternalShare externalShare,
-    EntryManager entryManager)
+    EntryManager entryManager,
+    IPSecurity.IPSecurity ipSecurity)
 {
     public async Task InvokeAsync(HttpContext context)
     {
@@ -85,7 +86,14 @@ public class FileHandlerService(FilesLinkUtility filesLinkUtility,
             //context.Response.StatusDescription = "Payment Required.";
             return;
         }
-
+        
+        if (authContext.IsAuthenticated && !(await ipSecurity.VerifyAsync()))
+        { 
+            context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            await context.Response.WriteAsync(Resource.ErrorIpSecurity);
+            return;
+        }
+        
         try
         {
             switch ((context.Request.Query[FilesLinkUtility.Action].FirstOrDefault() ?? "").ToLower())
