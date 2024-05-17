a29e4ffc410c98161bbad9a34a1e36bed1559b07
Andrey Savihin <andrey.savihin@onlyoffice.com>
2024-05-17 10:36:08 +0300

    ASC.Files: FileDeleteOperation: revert ProtoMembers order

--- products/ASC.Files/Core/Services/WCFService/FileOperations/FileDeleteOperation.cs
+++ products/ASC.Files/Core/Services/WCFService/FileOperations/FileDeleteOperation.cs
@@ -29,13 +29,13 @@ namespace ASC.Web.Files.Services.WCFService.FileOperations;
 [ProtoContract]
 public record FileDeleteOperationData<T> : FileOperationData<T>
 {
-    [ProtoMember(7)]
+    [ProtoMember(8)]
     public bool Immediately { get; set; }
     
-    [ProtoMember(8)]
+    [ProtoMember(9)]
     public bool IsEmptyTrash { get; set; }
 
-    [ProtoMember(9)]
+    [ProtoMember(10)]
     public bool HiddenOperation { get; set; }
 
     public FileDeleteOperationData()
