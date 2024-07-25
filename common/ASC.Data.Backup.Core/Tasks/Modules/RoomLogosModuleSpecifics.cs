fcfe8d8f26191d7e4d184102f8eb4f7c21847a31
Sukhorukov Anton <anton.sukhorukov@onlyoffice.com>
2024-07-25 18:08:05 +0300

    fix Bug 69384

--- common/ASC.Data.Backup.Core/Tasks/Modules/RoomLogosModuleSpecifics.cs
+++ common/ASC.Data.Backup.Core/Tasks/Modules/RoomLogosModuleSpecifics.cs
@@ -39,29 +39,30 @@ public class RoomLogosModuleSpecifics(Helpers helpers) : ModuleSpecificsBase(hel
         {
             var split = filePath.Split('_');
             var roomId = columnMapper.GetMapping("files_folder", "id", split[0]);
-
             if (roomId != null) 
             {
                 filePath = roomId + "_" + split[1];
                 return true;
             }
-            split = filePath.Split('-');
-            roomId = columnMapper.GetMapping("files_thirdparty_account", "id", split[1]);
-            if (roomId != null)
-            {
-                filePath = split[0] + "-" + roomId + "-" + split[2];
-                return true;
-            }
-            else
-            {
-                if (!dump)
-                {
-                    return false;
-                }
 
-                roomId = split[0];
-                return true;
+            split = filePath.Split('-');
+            if (split.Length > 1) 
+            {
+                roomId = columnMapper.GetMapping("files_thirdparty_account", "id", split[1]);
+                if (roomId != null)
+                {
+                    filePath = split[0] + "-" + roomId + "-" + split[2];
+                    return true;
+                }
             }
+
+            if (!dump)
+            {
+                return false;
+            }
+
+            roomId = split[0];
+            return true;
         }
         catch
         {
