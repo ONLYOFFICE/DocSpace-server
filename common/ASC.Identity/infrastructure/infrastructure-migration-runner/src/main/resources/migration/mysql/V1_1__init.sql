fc7be886288891a2fda0eca15527308ac935850e
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2024-08-13 18:59:46 +0500

    fix(migration): columns length

--- common/ASC.Identity/infrastructure/infrastructure-migration-runner/src/main/resources/migration/mysql/V1_1__init.sql
+++ common/ASC.Identity/infrastructure/infrastructure-migration-runner/src/main/resources/migration/mysql/V1_1__init.sql
@@ -41,7 +41,7 @@ CREATE TABLE identity_scopes (
 -- Create table for identity authorizations
 CREATE TABLE identity_authorizations (
     id varchar(255),
-    registered_client_id varchar(255) not null,
+    registered_client_id varchar(36) not null,
     principal_id varchar(255) not null,
     tenant_id integer not null,
     state varchar(500),
@@ -120,7 +120,7 @@ CREATE TABLE identity_client_scopes (
 -- Create table for identity consents
 CREATE TABLE identity_consents (
     principal_id varchar(255) not null,
-    registered_client_id varchar(255) not null,
+    registered_client_id varchar(36) not null,
     is_invalidated tinyint(1) default false,
     modified_at datetime(6),
     primary key (principal_id, registered_client_id),
--- common/ASC.Identity/infrastructure/infrastructure-migration-runner/src/main/resources/migration/mysql/V1_2__certs.sql
+++ common/ASC.Identity/infrastructure/infrastructure-migration-runner/src/main/resources/migration/mysql/V1_2__certs.sql
@@ -1,5 +1,5 @@
 CREATE TABLE identity_certs (
-	id varchar(255) not null,
+	id varchar(36) not null,
 	created_at datetime(6),
 	pair_type tinyint check (pair_type between 0 and 1) not null,
 	private_key text not null,
