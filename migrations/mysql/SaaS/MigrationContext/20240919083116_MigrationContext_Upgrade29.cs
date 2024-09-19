d67135fe03da7531cb5237faa02cb18949f85c71
Andrey Savihin <andrey.savihin@onlyoffice.com>
2024-09-19 15:59:32 +0300

    migrations: rename 20240819083116_MigrationContext_Upgrade28.cs to 20240919083116_MigrationContext_Upgrade29.cs

--- migrations/mysql/SaaS/MigrationContext/20240919083116_MigrationContext_Upgrade29.cs
+++ migrations/mysql/SaaS/MigrationContext/20240919083116_MigrationContext_Upgrade29.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace ASC.Migrations.MySql.SaaS.Migrations
 {
     /// <inheritdoc />
-    public partial class MigrationContext_Upgrade28 : Migration
+    public partial class MigrationContext_Upgrade29 : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
