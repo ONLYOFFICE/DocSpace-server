f513826b0604c9fa05d3df461b26dcf9ac6297cf
Andrey Savihin <andrey.savihin@onlyoffice.com>
2024-10-09 15:33:28 +0300

    Migrations: spam column moved from tenants table to users table

---
+++ migrations/mysql/SaaS/MigrationContext/20241009122957_MigrationContext_Upgrade33.cs
@@ -0,0 +1,68 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASC.Migrations.MySql.SaaS.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationContext_Upgrade33 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "spam",
+                table: "core_user",
+                type: "tinyint(1)",
+                nullable: true);
+
+           /*
+            migrationBuilder.UpdateData(
+                table: "core_user",
+                keyColumn: "id",
+                keyValue: "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
+                column: "spam",
+                value: null);
+           */
+
+            migrationBuilder.Sql("UPDATE core_user JOIN tenants_tenants ON core_user.id = tenants_tenants.owner_id SET core_user.spam = tenants_tenants.spam");
+
+            migrationBuilder.DropColumn(
+                name: "spam",
+                table: "tenants_tenants");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "spam",
+                table: "tenants_tenants",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValueSql: "'1'");
+
+            /*
+            migrationBuilder.UpdateData(
+                table: "tenants_tenants",
+                keyColumn: "id",
+                keyValue: -1,
+                columns: new string[0],
+                values: new object[0]);
+
+            migrationBuilder.UpdateData(
+                table: "tenants_tenants",
+                keyColumn: "id",
+                keyValue: 1,
+                columns: new string[0],
+                values: new object[0]);
+            */
+
+            migrationBuilder.Sql("UPDATE tenants_tenants JOIN core_user ON tenants_tenants.owner_id = core_user.id SET tenants_tenants.spam = core_user.spam");
+
+            migrationBuilder.DropColumn(
+                name: "spam",
+                table: "core_user");
+        }
+    }
+}
