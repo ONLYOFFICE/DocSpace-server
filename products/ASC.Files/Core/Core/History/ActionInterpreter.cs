7a6a5d2b8344a5afd5d9d725cf30d66de9b574b5
Andrey Savihin <andrey.savihin@onlyoffice.com>
2024-11-19 11:07:25 +0300

    fix for 3be9ae3ff795a561cde20acbbca81e0df0955578

--- products/ASC.Files/Core/Core/History/ActionInterpreter.cs
+++ products/ASC.Files/Core/Core/History/ActionInterpreter.cs
@@ -32,10 +32,8 @@ public abstract class ActionInterpreter
     {
         { MessageAction.FileMovedWithOverwriting, MessageAction.FileMoved },
         { MessageAction.FileCopiedWithOverwriting, MessageAction.FileCopied },
-        { MessageAction.FileMovedToTrash, MessageAction.FileMovedToTrash },
         { MessageAction.FolderMovedWithOverwriting, MessageAction.FolderMoved },
         { MessageAction.FolderCopiedWithOverwriting, MessageAction.FolderCopied },
-        { MessageAction.FolderMovedToTrash, MessageAction.FolderMovedToTrash },
         { MessageAction.FileRestoreVersion, MessageAction.UserFileUpdated },
         { MessageAction.FileUploadedWithOverwriting, MessageAction.UserFileUpdated }
     }.ToFrozenDictionary();
