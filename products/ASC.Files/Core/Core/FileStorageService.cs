5f0612f29a4144b98236bd72899335ab4fec5993
Maksim Chegulov <maksim.chegulov@onlyoffice.com>
2024-12-10 14:49:34 +0300

    decreased log level

--- products/ASC.Files/Core/Core/FileStorageService.cs
+++ products/ASC.Files/Core/Core/FileStorageService.cs
@@ -4310,7 +4310,7 @@ public class FileStorageService //: IFileStorageService
 
     private Exception GenerateException(Exception error, bool warning = false)
     {
-        if (warning)
+        if (warning || error is ItemNotFoundException or SecurityException or ArgumentException or TenantQuotaException or InvalidOperationException)
         {
             _logger.Information(error.ToString());
         }
