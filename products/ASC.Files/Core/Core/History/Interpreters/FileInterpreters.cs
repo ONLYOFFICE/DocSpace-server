0fe0d03ef29bb70e86a395f105e0a4bfedb3c8c6
pavelbannov <pavel.bannov@onlyoffice.com>
2025-01-31 18:52:39 +0300

    Add support for recording file version removal in history
    Introduced a new `FileVersionRemovedData` record to handle file version removal events. Updated related interpreter logic to utilize the new record and registered it for JSON serialization. This enhances tracking and management of file version history efficiently.

--- products/ASC.Files/Core/Core/History/Interpreters/FileInterpreters.cs
+++ products/ASC.Files/Core/Core/History/Interpreters/FileInterpreters.cs
@@ -199,6 +199,29 @@ public record FileIndexChangedData : EntryData
     }
 }
 
+public record FileVersionRemovedData : EntryData
+{
+    public int Version { get; }
+    
+    public FileVersionRemovedData(
+        string id,
+        string title,
+        int? parentId = null,
+        string parentTitle = null,
+        int? parentType = null,
+        string version = "") : base(id,
+        title,
+        parentId,
+        parentTitle,
+        parentType)
+    {
+        if (int.TryParse(version, out var versionParsed))
+        {
+            Version = versionParsed;
+        }
+    }
+}
+
 #endregion
 
 #region Interpreters
@@ -289,7 +312,7 @@ public class FileVersionDeletedInterpreter : ActionInterpreter
 {
     protected override ValueTask<HistoryData> GetDataAsync(IServiceProvider serviceProvider, string target, List<string> description, FileEntry<int> entry)
     {
-        return new ValueTask<HistoryData>(new EntryData(target, description[0]));
+        return new ValueTask<HistoryData>(new FileVersionRemovedData(target, description[0], version: description[1]));
     }
 }
 
