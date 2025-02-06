8678386a3e29982f22f7490bc341a95f21a5652d
Andrey Savihin <andrey.savihin@onlyoffice.com>
2025-02-06 17:26:27 +0300

    ASC.Common: DateTimeJsonConverter: formatting

--- common/ASC.Common/Utils/DateTimeJsonConverter.cs
+++ common/ASC.Common/Utils/DateTimeJsonConverter.cs
@@ -1,4 +1,4 @@
-﻿// (c) Copyright Ascensio System SIA 2009-2024
+// (c) Copyright Ascensio System SIA 2009-2024
 //
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
@@ -51,4 +51,3 @@ public class CustomFormatDateTimeJsonConverter(string format) : JsonConverter<Da
         writer.WriteStringValue(value.ToString(format));
     }
 }
-
