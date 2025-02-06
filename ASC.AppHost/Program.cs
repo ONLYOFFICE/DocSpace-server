6a3d7924f97dd1255d3b016b330588dd7873fe97
pavelbannov <pavel.bannov@onlyoffice.com>
2025-02-06 21:07:11 +0300

    Add MySQL integration and migration runner setup
    Introduced MySQL support via `Aspire.Hosting.MySql` and added a migration runner to manage database migrations. Updated service projects to include MySQL references and dependencies. Adjusted solution files to include the new migration runner project.

--- ASC.AppHost/Program.cs
+++ ASC.AppHost/Program.cs
@@ -26,38 +26,70 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var mySql = builder
+    .AddMySql("mysql")
+    .AddDatabase("docspace");
+
+var path = Path.GetFullPath(Path.Combine("..", "common", "Tools", "ASC.Migration.Runner", "bin", "Debug", "ASC.Migration.Runner.exe"));
+
+var migrate = builder
+    .AddExecutable("migrate",path, Path.GetDirectoryName(path))
+    .WithReference(mySql)
+    .WaitFor(mySql);
 
 builder.AddProject<Projects.ASC_ApiSystem>("asc-apisystem")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_ClearEvents>("asc-clearevents")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Data_Backup>("asc-data-backup")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Data_Backup_BackgroundTasks>("asc-data-backup-backgroundtasks")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
-builder.AddProject<Projects.ASC_Notify>("asc-notify");
+builder.AddProject<Projects.ASC_Notify>("asc-notify")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Web_Api>("asc-web-api")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_People>("asc-people")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Files>("asc-files")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Files_Service>("asc-files-service")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Studio_Notify>("asc-studio-notify")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddProject<Projects.ASC_Web_Studio>("asc-web-studio")
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .WithReference(mySql, "default:connectionString")
+    .WaitFor(migrate);
 
 builder.AddNodeApp("asc-socketIO", "server.js", "../common/ASC.Socket.IO/");
 
