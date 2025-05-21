799ab0d09e657aae5880a4ec1195c724cfe21798
Nikolay Rechkin <nikolay.rechkin@onlyoffice.com>
2025-05-21 13:51:57 +0300

    fix Bug 74460

--- products/ASC.Files/Core/Core/Dao/TeamlabDao/FileDao.cs
+++ products/ASC.Files/Core/Core/Dao/TeamlabDao/FileDao.cs
@@ -1215,8 +1215,11 @@ internal class FileDao(
         var file = await GetFileAsync(fileId);
         if (file != null)
         {
+            var status = await file.GetFileStatus();
+            var flagsToRemove = FileStatus.IsEditing | FileStatus.IsEditingAlone | FileStatus.IsConverting;
+
             var copy = _serviceProvider.GetService<File<int>>();
-            copy.SetFileStatus(await file.GetFileStatus() ^ FileStatus.IsEditing ^ FileStatus.IsEditingAlone ^ FileStatus.IsConverting);
+            copy.SetFileStatus(status & ~flagsToRemove);
             copy.ParentId = toFolderId;
             copy.Title = file.Title;
             copy.ConvertedType = file.ConvertedType;
