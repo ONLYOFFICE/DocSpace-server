8b0805f76a1e5bbe9975f20806505b46f74a3e4b
Andrey Savihin <andrey.savihin@onlyoffice.com>
2025-05-29 11:55:29 +0300

    ASC.Core.Common: MessageEvent: increase column size for page
    fix ArgumentException: The field Page must be a string or array type with a maximum length of '300'

---
+++ migrations/mysql/SaaS/MigrationContext/20250529085248_MigrationContext_Upgrade54.cs
@@ -0,0 +1,78 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASC.Migrations.MySql.SaaS.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationContext_Upgrade54 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "login_events",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                collation: "utf8_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true,
+                oldCollation: "utf8_general_ci")
+                .Annotation("MySql:CharSet", "utf8")
+                .OldAnnotation("MySql:CharSet", "utf8");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "audit_events",
+                type: "varchar(1000)",
+                maxLength: 1000,
+                nullable: true,
+                collation: "utf8_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true,
+                oldCollation: "utf8_general_ci")
+                .Annotation("MySql:CharSet", "utf8")
+                .OldAnnotation("MySql:CharSet", "utf8");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "login_events",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: true,
+                collation: "utf8_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true,
+                oldCollation: "utf8_general_ci")
+                .Annotation("MySql:CharSet", "utf8")
+                .OldAnnotation("MySql:CharSet", "utf8");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "page",
+                table: "audit_events",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: true,
+                collation: "utf8_general_ci",
+                oldClrType: typeof(string),
+                oldType: "varchar(1000)",
+                oldMaxLength: 1000,
+                oldNullable: true,
+                oldCollation: "utf8_general_ci")
+                .Annotation("MySql:CharSet", "utf8")
+                .OldAnnotation("MySql:CharSet", "utf8");
+        }
+    }
+}
