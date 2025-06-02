a735ce41046e4f07e5ed090d6577a3810cb8df14
pavelbannov <pavel.bannov@onlyoffice.com>
2025-06-02 14:30:10 +0300

    Refactor caching tags in `CachedUserService` and `CacheExtention` to use consistent method `GetGroupRefsTag`.

--- common/ASC.Core.Common/Caching/CachedUserService.cs
+++ common/ASC.Core.Common/Caching/CachedUserService.cs
@@ -271,13 +271,7 @@ public class CachedUserService : IUserService
         {
             var refs = await _service.GetUserGroupRefsAsync(tenant);
 
-            var tags = new List<string>();
-            foreach (var r in refs)
-            {
-                tags.Add(CacheExtention.GetGroupRefTag(tenant, r.Value.GroupId));
-            }
-
-            ctx.Tags = tags.ToArray();
+            ctx.Tags = [CacheExtention.GetGroupRefsTag(tenant)];
 
             return ctx.Modified(new UserGroupRefStore(refs));
         }, _cacheExpiration);
@@ -293,7 +287,7 @@ public class CachedUserService : IUserService
         {
             var groupRef = await _service.GetUserGroupRefAsync(tenant, groupId, refType);
 
-            ctx.Tags = [CacheExtention.GetGroupRefTag(tenant, groupId)];
+            ctx.Tags = [CacheExtention.GetGroupRefsTag(tenant)];
 
             return ctx.Modified(groupRef, lastModified: DateTime.UtcNow);
         }, _cacheExpiration);
@@ -305,7 +299,7 @@ public class CachedUserService : IUserService
     {
         r = await _service.SaveUserGroupRefAsync(tenant, r);
 
-        var tag = CacheExtention.GetGroupRefTag(tenant, r.GroupId);
+        var tag = CacheExtention.GetGroupRefsTag(tenant);
         await _cache.RemoveByTagAsync(tag);
 
         return r;
@@ -317,7 +311,7 @@ public class CachedUserService : IUserService
 
         var r = new UserGroupRef(userId, groupId, refType) { TenantId = tenant, Removed = true };
 
-        var tag = CacheExtention.GetGroupRefTag(tenant, groupId);
+        var tag = CacheExtention.GetGroupRefsTag(tenant);
         await _cache.RemoveByTagAsync(tag);
     }
 
