0a54ade69660371804505e357930064a9f844b7c
Andrei Panov <andrew.panov@onlyoffice.com>
2025-07-16 14:25:06 +0300

    feat(migrations): add username to telegram user

---
+++ migrations/mysql/SaaS/MigrationContext/20250716093816_MigrationContext_Upgrade55.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ASC.Migrations.MySql.SaaS.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationContext_Upgrade55 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "telegram_username",
+                table: "telegram_users",
+                type: "varchar(35)",
+                nullable: true,
+                collation: "utf8_general_ci")
+                .Annotation("MySql:CharSet", "utf8");
+
+            migrationBuilder.CreateIndex(
+                name: "tgUsername",
+                table: "telegram_users",
+                column: "telegram_username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "tgUsername",
+                table: "telegram_users");
+
+            migrationBuilder.DropColumn(
+                name: "telegram_username",
+                table: "telegram_users");
+        }
+    }
+}
