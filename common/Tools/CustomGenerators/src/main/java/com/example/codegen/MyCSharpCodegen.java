d3dac93eb2e3a69c689bc6f48eb57fb016a2cf06
diana-vahomskaya <diana.vahomskaya@onlyoffice.com>
2025-07-31 11:33:59 +0300

    fixed names for api methods

--- common/Tools/CustomGenerators/src/main/java/com/example/codegen/MyCSharpCodegen.java
+++ common/Tools/CustomGenerators/src/main/java/com/example/codegen/MyCSharpCodegen.java
@@ -3,6 +3,8 @@ package com.example.codegen;
 import io.swagger.v3.oas.models.servers.ServerVariables;
 import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.servers.ServerVariable;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.tags.Tag;
 
 import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
@@ -14,13 +16,25 @@ import org.openapitools.codegen.model.OperationMap;
 import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.CodegenParameter;
 import org.openapitools.codegen.CodegenProperty;
+import static org.openapitools.codegen.utils.CamelizeOption.LOWERCASE_FIRST_LETTER;
+import static org.openapitools.codegen.utils.StringUtils.camelize;
+import org.openapitools.codegen.model.ApiInfoMap;
 
 import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.HashMap;
+import com.example.codegen.TagParts;
+import java.util.Locale;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 public class MyCSharpCodegen extends CSharpClientCodegen {
+    
+    protected String apiNamePrefix = "", apiNameSuffix = "Api";
 
     public MyCSharpCodegen() {
         super();
@@ -53,11 +67,11 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
         );
 
         supportingFiles.add(new SupportingFile(
-            "Program.mustache", packageFolder + ".Example", "Program.cs"
+            "Program.mustache", "samples" + File.separator + packageName + ".Example", "Program.cs"
         ));
 
         supportingFiles.add(new SupportingFile(
-            "ExampleProject.mustache", packageFolder + ".Example", packageName + ".Example.csproj"
+            "ExampleProject.mustache", "samples" + File.separator + packageName + ".Example", packageName + ".Example.csproj"
         ));
 
         supportingFiles.add(new SupportingFile(
@@ -75,7 +89,6 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
         supportingFiles.add(new SupportingFile(
             "CHANGELOG.mustache", "", "CHANGELOG.md"
         ));
-
     }
 
     @Override
@@ -84,6 +97,7 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
 
         for (ModelMap mo : objs.getModels()) {
             CodegenModel model = mo.getModel();
+
             if ("ApiDateTime".equals(model.classname)) {
                 model.vendorExtensions.put("isApiDateTime", true);
 
@@ -118,6 +132,13 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
         if (objs != null && objs.getOperations() != null) {
             OperationMap operationMap = objs.getOperations();
             List<CodegenOperation> operationList = operationMap.getOperation();
+            String className = operationMap.getClassname();
+            if (className != null && className.endsWith(apiNameSuffix)) {
+                className = className.substring(0, className.length() - 3);
+            }
+            TagParts tagParts = tagMap.get(camelize(className));
+            operationMap.put("x-folder", tagParts.folderPart);
+            operationMap.put("x-classname", tagParts.classPart + apiNameSuffix);
             if (operationList != null) {
                 for (CodegenOperation op : operationList) { 
                     if (op.operationId != null) {
@@ -190,6 +211,35 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
     public Map<String, Object> postProcessSupportingFileData(Map<String, Object> objs) {
         super.postProcessSupportingFileData(objs);
 
+        ApiInfoMap apiInfo = (ApiInfoMap) objs.get("apiInfo");
+        Map<String, List<Map<String, Object>>> folderToApis = new LinkedHashMap<>();
+        for (OperationsMap api : apiInfo.getApis()) {
+
+            OperationMap operationMap = api.getOperations();
+            String className = operationMap.getClassname();
+            if (className != null && className.endsWith(apiNameSuffix)) {
+                className = className.substring(0, className.length() - 3);
+            }
+            TagParts tagParts = tagMap.get(camelize(className));
+
+            String folder = tagParts.folderPart;
+            String classname = tagParts.classPart + apiNameSuffix;
+            api.put("x-folder", folder);
+            api.put("x-classname", classname);
+
+            folderToApis.computeIfAbsent(folder, k -> new ArrayList<>()).add(api);
+        }
+
+        List<Map<String, Object>> customApis = new ArrayList<>();
+        for (Map.Entry<String, List<Map<String, Object>>> entry : folderToApis.entrySet()) {
+            Map<String, Object> folderEntry = new HashMap<>();
+            folderEntry.put("folder", entry.getKey());
+            folderEntry.put("apis", entry.getValue());
+            customApis.add(folderEntry);
+        }
+
+        objs.put("customApis", customApis);
+
         objs.put("x-authorizationUrl", "{{authBaseUrl}}/oauth2/authorize");
         objs.put("x-tokenUrl", "{{authBaseUrl}}/oauth2/token");
         objs.put("x-openIdConnectUrl", "{{authBaseUrl}}/.well-known/openid-configuration");
@@ -217,4 +267,66 @@ public class MyCSharpCodegen extends CSharpClientCodegen {
     public String getHelp() {
         return "Generates a custom Csharp client";
     }
+
+    private final Map<String, TagParts> tagMap = new HashMap<>();
+
+    @Override
+    public String sanitizeTag(String tag) {
+        String sanitized = super.sanitizeTag(tag);
+        if (!tagMap.containsKey(sanitized)) {
+            String[] parts = tag.split(" / ");
+            String folderPart = parts[0];
+            String classPart = (parts.length > 1) ? parts[1] : parts[0];
+
+            TagParts info = new TagParts(
+                tag,
+                camelize(sanitizeName(folderPart)),
+                camelize(sanitizeName(classPart))
+            );
+
+            tagMap.put(sanitized, info);
+        }
+        return sanitized;
+    }
+
+    private final Map<String, String> seenApiFilenames = new HashMap<String, String>();
+
+    @Override
+    public String apiFilename(String templateName, String tag) {
+        String uniqueTag = uniqueCaseInsensitiveString(tag, seenApiFilenames);
+        String suffix = apiTemplateFiles().get(templateName);
+        TagParts tagParts = tagMap.get(camelize(uniqueTag));
+        if (tagParts == null) {
+            return apiFileFolder() + File.separator + toApiFilename(uniqueTag) + suffix;
+        }
+
+        String folderPath = apiFileFolder() + File.separator + tagParts.folderPart;
+        String filename = toApiFilename(tagParts.classPart) + suffix;
+
+        return folderPath + File.separator + filename;
+    }
+
+    private String uniqueCaseInsensitiveString(String value, Map<String, String> seenValues) {
+        if (seenValues.keySet().contains(value)) {
+            return seenValues.get(value);
+        }
+
+        Optional<Entry<String, String>> foundEntry = seenValues.entrySet().stream().filter(v -> v.getValue().toLowerCase(Locale.ROOT).equals(value.toLowerCase(Locale.ROOT))).findAny();
+        if (foundEntry.isPresent()) {
+            int counter = 0;
+            String uniqueValue = value + "_" + counter;
+
+            while (seenValues.values().stream().map(v -> v.toLowerCase(Locale.ROOT)).collect(Collectors.toList()).contains(uniqueValue.toLowerCase(Locale.ROOT))) {
+                counter++;
+                uniqueValue = value + "_" + counter;
+            }
+
+            seenValues.put(value, uniqueValue);
+            return uniqueValue;
+        }
+
+        seenValues.put(value, value);
+        return value;
+    }
+
 }
