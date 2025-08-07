1de5ca18c530be448b6794a4a46b5ee968da9abb
Andrey Savihin <andrey.savihin@onlyoffice.com>
2025-08-07 16:19:30 +0300

    Revert "revert a6bb9aac836bffa0d18092853b5bdf91424b5028"
    This reverts commit e823ba4a6471599c8903b4d84fd4cf8a7705c30b.

--- web/ASC.Web.Api/Api/Settings/WhitelabelController.cs
+++ web/ASC.Web.Api/Api/Settings/WhitelabelController.cs
@@ -221,6 +221,11 @@ public class WhitelabelController(
 
         foreach (var logoType in Enum.GetValues<WhiteLabelLogoType>())
         {
+            if (logoType == WhiteLabelLogoType.Notification)
+            {
+                continue;
+            }
+
             var result = new WhiteLabelItemDto
             {
                 Type = logoType,
