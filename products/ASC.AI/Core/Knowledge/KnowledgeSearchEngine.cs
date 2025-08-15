9827d19352c53ff1a347f5493318db2f47ef6f3b
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2025-08-15 16:32:07 +0300

    added knowledge search engine

---
+++ products/ASC.AI/Core/Knowledge/KnowledgeSearchEngine.cs
@@ -0,0 +1,64 @@
+// (c) Copyright Ascensio System SIA 2009-2025
+// 
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+// 
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+// 
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+// 
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+// 
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+// 
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+using ASC.ElasticSearch.VectorData;
+using ASC.Files.Core.Vectorization.Data;
+using ASC.Files.Core.Vectorization.Embedding;
+
+namespace ASC.AI.Core.Knowledge;
+
+[Scope]
+public class KnowledgeSearchEngine(
+    TenantManager tenantManager,
+    VectorStore vectorStore,
+    EmbeddingGeneratorFactory embeddingGeneratorFactory)
+{
+    public async Task<IEnumerable<Knowledge>> SearchAsync(int roomId, string query)
+    {
+        var tenantId = tenantManager.GetCurrentTenantId();
+        
+        var generator = embeddingGeneratorFactory.Create();
+        var embedding = await generator.GenerateAsync(query);
+        
+        var collection = vectorStore.GetCollection<Chunk>(Chunk.IndexName, null);
+        var searchOptions = new VectorSearchOptions<Chunk>
+        {
+            Filter = x => x.TenantId == tenantId && x.RoomId == roomId
+        };
+        
+        var chunks = await collection.SearchAsync(
+            x => x.Embedding,
+            embedding.Vector.ToArray(),
+            5,
+            searchOptions).ToListAsync();
+
+        return chunks.Select(x => new Knowledge
+        {
+            FileId = x.FileId,
+            Text = x.TextEmbedding
+        });
+    }
+}
