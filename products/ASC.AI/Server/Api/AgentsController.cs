0b86ce406b91a25fae4dd5658da59269568272bb
Andrei Panov <andrew.panov@onlyoffice.com>
2025-10-18 15:15:20 +0300

    feat: add method for getting ai agents list

--- products/ASC.AI/Server/Api/AgentsController.cs
+++ products/ASC.AI/Server/Api/AgentsController.cs
@@ -24,10 +24,14 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
+using ASC.Api.Core.Core;
 using ASC.Api.Utils;
+using ASC.Files.Core;
 using ASC.Files.Core.ApiModels;
 using ASC.Files.Core.ApiModels.RequestDto;
+using ASC.Files.Core.ApiModels.ResponseDto;
 using ASC.Files.Core.VirtualRooms;
+using ASC.Web.Files.Classes;
 using ASC.Web.Files.Services.WCFService;
 using ASC.Web.Files.Services.WCFService.FileOperations;
 
@@ -44,9 +48,65 @@ namespace ASC.AI.Api
         FileStorageService fileStorageService,
         FolderDtoHelper folderDtoHelper,
         FileDeleteOperationsManager fileDeleteOperationsManager,
-        FileOperationDtoHelper fileOperationDtoHelper)
+        FileOperationDtoHelper fileOperationDtoHelper,
+        GlobalFolderHelper globalFolderHelper,
+        FolderContentDtoHelper folderContentDtoHelper)
         : ControllerBase
     {
+        /// <summary>
+        /// Get ai agents
+        /// </summary>
+        /// <short>Get ai agents</short>
+        /// <path>api/2.0/ai/agents</path>
+        [SwaggerResponse(200, "Agent information", typeof(FolderContentDto<int>))]
+        [HttpGet("agents")]
+        public async Task<FolderContentDto<int>> GetAgents(GetAgentListRequestDto inDto)
+        {
+            var parentId = await globalFolderHelper.GetFolderAiAgentsAsync();
+
+            HashSet<FilterType> filter = [FilterType.AiRooms];
+
+            var tagNames = !string.IsNullOrEmpty(inDto.Tags)
+                ? JsonSerializer.Deserialize<IEnumerable<string>>(inDto.Tags)
+                : null;
+
+            OrderBy orderBy = null;
+            if (SortedByTypeExtensions.TryParse(inDto.SortBy, true, out var sortBy))
+            {
+                orderBy = new OrderBy(sortBy, inDto.SortOrder == SortOrder.Ascending);
+            }
+
+            var startIndex = inDto.StartIndex;
+            var count = inDto.Count;
+            var filterValue = inDto.Text;
+
+            var content = await fileStorageService.GetFolderItemsAsync(
+                parentId,
+                startIndex,
+                count,
+                filter,
+                false,
+                inDto.SubjectId,
+                filterValue,
+                [],
+                true,
+                false,
+                orderBy,
+                SearchArea.AiAgents,
+                0,
+                inDto.WithoutTags ?? false,
+                tagNames,
+                inDto.ExcludeSubject ?? false,
+                ProviderFilter.None,
+                inDto.SubjectFilter ?? SubjectFilter.Owner,
+                quotaFilter: inDto.QuotaFilter ?? QuotaFilter.All,
+                storageFilter: StorageFilter.None);
+
+            var dto = await folderContentDtoHelper.GetAsync(parentId, content, startIndex);
+
+            return dto.NotFoundIfNull();
+        }
+
         /// <summary>
         /// Creates an ai agent.
         /// </summary>
@@ -54,7 +114,7 @@ namespace ASC.AI.Api
         /// <path>api/2.0/ai/agents</path>
         [SwaggerResponse(200, "Agent information", typeof(FolderDto<int>))]
         [HttpPost("agents")]
-        public async Task<FolderDto<int>> CreateRoom(CreateRoomRequestDto inDto)
+        public async Task<FolderDto<int>> CreateAgent(CreateAgentRequestDto inDto)
         {
             var lifetime = mapper.Map<RoomDataLifetimeDto, RoomDataLifetime>(inDto.Lifetime);
             if (lifetime != null)
@@ -64,9 +124,12 @@ namespace ASC.AI.Api
 
             var room = await fileStorageService.CreateRoomAsync(inDto.Title, RoomType.AiRoom, inDto.Private,
                 inDto.Indexing, inDto.Share, inDto.Quota, lifetime, inDto.DenyDownload, inDto.Watermark, inDto.Color, inDto.Cover,
-                inDto.Tags, inDto.Logo, new ChatSettings() {
-                    ProviderId = inDto.ChatSettings.ProviderId, Prompt = inDto.ChatSettings.Prompt,
-                    ModelId = inDto.ChatSettings.ModelId, IsAgent = true
+                inDto.Tags, inDto.Logo, new ChatSettings()
+                {
+                    ProviderId = inDto.ChatSettings.ProviderId,
+                    Prompt = inDto.ChatSettings.Prompt,
+                    ModelId = inDto.ChatSettings.ModelId,
+                    IsAgent = true
                 });
 
             return await folderDtoHelper.GetAsync(room);
@@ -79,7 +142,7 @@ namespace ASC.AI.Api
         /// <path>api/2.0/ai/agents/{id}</path>
         [SwaggerResponse(200, "Agent information", typeof(FolderDto<int>))]
         [HttpGet("agents/{id}")]
-        public async Task<FolderDto<int>> GetRoomInfo(RoomIdRequestDto<int> inDto)
+        public async Task<FolderDto<int>> GetAgentInfo(RoomIdRequestDto<int> inDto)
         {
             var folder = await fileStorageService.GetFolderAsync(inDto.Id).NotFoundIfNull("Folder not found");
 
@@ -93,7 +156,7 @@ namespace ASC.AI.Api
         /// <path>api/2.0/ai/agents/{id}</path>
         [SwaggerResponse(200, "Updated agent information", typeof(FolderDto<int>))]
         [HttpPut("agents/{id}")]
-        public async Task<FolderDto<int>> UpdateRoom(UpdateRoomRequestDto<int> inDto)
+        public async Task<FolderDto<int>> UpdateAgent(UpdateRoomRequestDto<int> inDto)
         {
             var room = await fileStorageService.UpdateRoomAsync(inDto.Id, inDto.UpdateRoom);
 
@@ -107,7 +170,7 @@ namespace ASC.AI.Api
         /// <path>api/2.0/ai/agents/{id}</path>
         [SwaggerResponse(200, "File operation", typeof(FileOperationDto))]
         [HttpDelete("agents/{id}")]
-        public async Task<FileOperationDto> DeleteRoom(DeleteRoomRequestDto<int> inDto)
+        public async Task<FileOperationDto> DeleteAgent(DeleteRoomRequestDto<int> inDto)
         {
             await fileDeleteOperationsManager.Publish([inDto.Id], [], false, !inDto.DeleteRoom.DeleteAfter, true);
 
