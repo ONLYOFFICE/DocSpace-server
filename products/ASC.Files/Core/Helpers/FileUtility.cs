788979354d644c38a25699fc0359157795b0f390
pavelbannov <pavel.bannov@onlyoffice.com>
2025-10-23 21:28:33 +0300

    Fix incorrect handling of file extensions in `FileUtility`

--- products/ASC.Files/Core/Helpers/FileUtility.cs
+++ products/ASC.Files/Core/Helpers/FileUtility.cs
@@ -425,7 +425,7 @@ public class FileUtility(
                     _extsConvertible[input] = [];
                 }
 
-                _extsConvertible[input].Add(output);
+                _extsConvertible[input].Add($".{output.TrimStart('.')}");
             }
         }
 
