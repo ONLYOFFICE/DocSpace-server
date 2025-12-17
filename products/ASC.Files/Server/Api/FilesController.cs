fd50d3323c64e38af15ebc110e03a788cb4a18e5
Nikolay Rechkin <nikolay.rechkin@onlyoffice.com>
2025-12-17 16:05:14 +0300

    Files: Form: add API to get completed form data

--- products/ASC.Files/Server/Api/FilesController.cs
+++ products/ASC.Files/Server/Api/FilesController.cs
@@ -627,6 +627,20 @@ public abstract class FilesController<T>(
     {
         await fileStorageService.ManageFormFilling(inDto.FormId, inDto.Action);
     }
+
+    /// <summary>
+    /// Returns the results of form submissions.
+    /// </summary>
+    /// <short>Get form submission results</short>
+    /// <path>api/2.0/files/file/{fileId}/submissions</path>
+    [Tags("Files / Files")]
+    [SwaggerResponse(200, "Form submission results were successfully retrieved")]
+    [SwaggerResponse(403, "You do not have enough permissions to perform this action")]
+    [HttpGet("file/{fileId}/submissions")]
+    public IAsyncEnumerable<FormResultsDto> GetFormSubmissions(FileIdRequestDto<int> inDto)
+    {
+        return fileStorageService.GetSubmissionsByFormId(inDto.FileId);
+    }
 }
 
 public class FilesControllerCommon(
