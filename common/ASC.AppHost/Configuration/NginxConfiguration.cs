698d245a31e3477c9358d1231a8f2bbe5243a741
alexeybannov <alexey.bannov@onlyoffice.com>
2026-01-27 11:08:48 +0300

    aspire: use template for nginx upstream config to avoid git changes

--- common/ASC.AppHost/Configuration/NginxConfiguration.cs
+++ common/ASC.AppHost/Configuration/NginxConfiguration.cs
@@ -54,8 +54,7 @@ public static class NginxConfiguration
         }
 
         openResty.WithArgs("/bin/sh", "-c",
-            $"envsubst '{string.Join(',', serviceUrls.Select(r => $"${r.Key}"))}' < /etc/nginx/includes/onlyoffice-upstream-map.conf > /etc/nginx/includes/onlyoffice-upstream.conf && " +
-            $"mv /etc/nginx/includes/onlyoffice-upstream.conf /etc/nginx/includes/onlyoffice-upstream-map.conf && " +
+            $"envsubst '{string.Join(',', serviceUrls.Select(r => $"${r.Key}"))}' < /etc/nginx/includes/onlyoffice-upstream-map.conf.template > /etc/nginx/includes/onlyoffice-upstream-map.conf && " +
             $"/usr/local/openresty/bin/openresty -g 'daemon off;'");
 
         return openResty;
