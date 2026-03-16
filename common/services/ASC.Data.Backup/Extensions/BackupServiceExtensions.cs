021b92c74e1eecf8821c3d07325463e7a16b934c
alexeybannov <alexey.bannov@onlyoffice.com>
2026-03-16 23:04:33 +0300

    ASC.Monolith: refactoring and optimization work

--- common/services/ASC.Data.Backup/Extensions/BackupServiceExtensions.cs
+++ common/services/ASC.Data.Backup/Extensions/BackupServiceExtensions.cs
@@ -1,55 +1,65 @@
-﻿// (c) Copyright Ascensio System SIA 2009-2026
-// 
+// (c) Copyright Ascensio System SIA 2009-2026
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-using ASC.Web.Api.Extensions;
+using ASC.Data.Backup.Core.Quota;
+using ASC.Files.Core.Core;
 
-namespace ASC.Web.Api;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-public class Startup : BaseStartup
+namespace ASC.Data.Backup.Extensions;
+
+public static class BackupServiceExtensions
 {
-    public Startup(IConfiguration configuration) : base(configuration)
+    public static IServiceCollection AddBackupServices(this IServiceCollection services)
     {
-        if (configuration.GetSection("RabbitMQ").GetChildren().Any() &&
-            String.IsNullOrEmpty(configuration["RabbitMQ:ClientProvidedName"]))
+        var maxRequestLimit = 1024L * 1024L * 1024L;
+        services.Configure<KestrelServerOptions>(options =>
         {
-            configuration["RabbitMQ:ClientProvidedName"] = Program.AppName;
-        }
+            options.Limits.MaxRequestBodySize = maxRequestLimit;
+        });
+        services.Configure<FormOptions>(x =>
+        {
+            x.MultipartBodyLengthLimit = maxRequestLimit;
+        });
+
+        services.AddBaseDbContextPool<BackupsContext>();
+        services.AddBaseDbContextPool<FilesDbContext>();
+        services.RegisterQuotaFeature();
+        services.RegisterFreeBackupQuotaFeature();
+
+        return services;
     }
 
-    public override async Task ConfigureServices(WebApplicationBuilder builder)
+    public static IApplicationBuilder UseBackupMiddleware(this IApplicationBuilder app)
     {
-        await base.ConfigureServices(builder);
+        app.MapWhen(
+            context => context.Request.Path.ToString().EndsWith("backupFileUpload.ashx"),
+            appBranch => appBranch.UseBackupFileUploadHandler());
 
-        builder.Services.AddWebApiServices(_configuration);
-    }
-
-    public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        base.Configure(app, env);
-
-        app.UseWebApiMiddleware();
+        return app;
     }
 }
