88342f0ed8c9584b439df4d18a597d738b973f39
Andrei Panov <andrew.panov@onlyoffice.com>
2026-03-23 13:33:35 +0300

    tests: add subjectfilter test for files

--- products/ASC.Files/Tests/Tests/03_Rooms/RoomsApiTests.cs
+++ products/ASC.Files/Tests/Tests/03_Rooms/RoomsApiTests.cs
@@ -24,6 +24,7 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
+using ASC.Files.Core.Security;
 using ASC.Files.Tests.ApiFactories;
 
 namespace ASC.Files.Tests.Tests._03_Rooms;
@@ -241,7 +242,57 @@ public class RoomsApiTests(
         roomsFolder.Folders.Should().NotBeEmpty();
         //roomsFolder.Folders.Should().Contain(f => f.Type == FolderType.CustomRoom);
     }
-    
+
+    [Fact]
+    public async Task GetRoomsFolderSubjectFilter_ReturnsFilteredRooms()
+    {
+        // Arrange
+        await _filesClient.Authenticate(Initializer.Owner);
+        var roomAdmin = await Initializer.InviteContact(EmployeeType.RoomAdmin);
+
+        // Create an owner room and share it
+        var ownerRoom = await CreateCustomRoom("Owner room " + Guid.NewGuid().ToString()[..8]);
+        await _roomsApi.SetRoomSecurityAsync(ownerRoom.Id, new RoomInvitationRequest
+        {
+            Invitations =
+            [
+                new RoomInvitation { Id = roomAdmin.Id, Access = FileShare.Read }
+            ]
+        }, TestContext.Current.CancellationToken);
+
+        await _filesClient.Authenticate(roomAdmin);
+        var adminRoom = CreateCustomRoom("Admin room" + Guid.NewGuid().ToString()[..8]);
+
+        // Act
+        var ownerMemberRooms = (await _roomsApi.GetRoomsFolderAsync(
+            type: [RoomType.CustomRoom], subjectId: Initializer.Owner.Id.ToString(), subjectFilter: SubjectFilter.Member,
+            cancellationToken: TestContext.Current.CancellationToken)).Response;
+        var adminMemberRooms = (await _roomsApi.GetRoomsFolderAsync(
+            type: [RoomType.CustomRoom], subjectId: roomAdmin.Id.ToString(), subjectFilter: SubjectFilter.Member,
+            cancellationToken: TestContext.Current.CancellationToken)).Response;
+
+        var ownerOwnedRooms = (await _roomsApi.GetRoomsFolderAsync(
+            type: [RoomType.CustomRoom], subjectId: Initializer.Owner.Id.ToString(), subjectFilter: SubjectFilter.Owner,
+            cancellationToken: TestContext.Current.CancellationToken)).Response;
+        var adminOwnedRooms = (await _roomsApi.GetRoomsFolderAsync(
+            type: [RoomType.CustomRoom], subjectId: roomAdmin.Id.ToString(), subjectFilter: SubjectFilter.Owner,
+            cancellationToken: TestContext.Current.CancellationToken)).Response;
+
+        // Assert
+        ownerMemberRooms.Should().NotBeNull();
+        ownerMemberRooms.Folders.Should().HaveCount(1);
+
+        adminMemberRooms.Should().NotBeNull();
+        adminMemberRooms.Folders.Should().HaveCount(2);
+
+
+        ownerOwnedRooms.Should().NotBeNull();
+        ownerOwnedRooms.Folders.Should().HaveCount(1);
+
+        adminOwnedRooms.Should().NotBeNull();
+        adminOwnedRooms.Folders.Should().HaveCount(1);
+    }
+
     // [Fact]
     // public async Task SetRoomSecurity_InviteUser_AccessGranted()
     // {
