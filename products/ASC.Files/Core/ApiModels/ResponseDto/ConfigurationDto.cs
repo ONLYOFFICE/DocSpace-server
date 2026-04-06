efe618cd3ced50dee0da2edf3a6514a5115fbebd
pavel.bannov <pavel.bannov@onlyoffice.com>
2026-04-06 15:26:25 +0300

    fix bug 80495

--- products/ASC.Files/Core/ApiModels/ResponseDto/ConfigurationDto.cs
+++ products/ASC.Files/Core/ApiModels/ResponseDto/ConfigurationDto.cs
@@ -1,25 +1,25 @@
 ﻿// (c) Copyright Ascensio System SIA 2009-2026
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -606,7 +606,7 @@ public class ConfigurationConverter<T>(
 
 [Scope(GenericArguments = [typeof(int)])]
 [Scope(GenericArguments = [typeof(string)])]
-public class EditorConfigurationConverter<T>(CustomizationConfigConverter<T> configConverter)
+public class EditorConfigurationConverter<T>(CustomizationConfigConverter<T> configConverter, AuthContext authContext)
 {
     public async Task<EditorConfigurationDto> Convert(Configuration<T> configuration, File<T> file)
     {
@@ -632,7 +632,7 @@ public class EditorConfigurationConverter<T>(CustomizationConfigConverter<T> con
             Plugins = source.Plugins,
             Recent = await source.GetRecent(fileType, file.Id).ToListAsync(),
             Templates = [], // await source.GetTemplates(fileType, configuration.Document.Title),
-            User = await source.GetUserAsync()
+            User = authContext.IsAuthenticated ? await source.GetUserAsync() : null
         };
 
         return result;
@@ -790,7 +790,7 @@ public class DocumentConfigConverter<T>(InfoConfigConverter<T> configConverter, 
 
 [Scope(GenericArguments = [typeof(int)])]
 [Scope(GenericArguments = [typeof(string)])]
-public class InfoConfigConverter<T>
+public class InfoConfigConverter<T>(AuthContext authContext)
 {
     public async Task<InfoConfigDto> Convert(InfoConfig<T> source, File<T> file)
     {
@@ -803,7 +803,7 @@ public class InfoConfigConverter<T>
         {
             Favorite = await source.GetFavorite(file),
             Folder = await source.GetFolder(file),
-            Owner = source.GetOwner(file),
+            Owner = authContext.IsAuthenticated ? source.GetOwner(file) : null,
             SharingSettings = await source.GetSharingSettings(file),
             Type = source.Type,
             Uploaded = source.GetUploaded(file)
