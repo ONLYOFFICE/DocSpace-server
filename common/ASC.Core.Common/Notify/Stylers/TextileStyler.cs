1311b4ba931e7e594dd4ce59ab62881bd3749315
Andrey Savihin <andrey.savihin@onlyoffice.com>
2026-04-08 11:36:38 +0300

    fix Bug 80629

--- common/ASC.Core.Common/Notify/Stylers/TextileStyler.cs
+++ common/ASC.Core.Common/Notify/Stylers/TextileStyler.cs
@@ -1,25 +1,25 @@
 // (c) Copyright Ascensio System SIA 2009-2026
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -75,7 +75,7 @@ public class TextileStyler(CoreBaseSettings coreBaseSettings,
         if (!content.StartsWith("<tr"))
         {
             content = $@"<tr border=""0"" cellspacing=""0"" cellpadding=""0"">
-                <td style=""padding: 0 40px; margin: 0; text-align: center; vertical-align: top; width: 600px;"">{content}</td>
+                <td style=""padding: 0 40px; margin: 0; text-align: center; vertical-align: top; width: 600px; max-width: 600px;"">{content}</td>
             </tr>";
         }
 
