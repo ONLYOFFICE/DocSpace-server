03aeb3583faadfce115d9d16c84c51f239c40d79
Andrey Savihin <andrey.savihin@onlyoffice.com>
2026-04-09 18:42:45 +0300

    fix Bug 80980

--- web/ASC.Web.Studio/WebHooks/WebhookSender.cs
+++ web/ASC.Web.Studio/WebHooks/WebhookSender.cs
@@ -1,25 +1,25 @@
 ﻿// (c) Copyright Ascensio System SIA 2009-2026
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -67,8 +67,24 @@ public class WebhookSender(
 
             var entry = await dbWorker.ReadJournal(webhookRequest.TenantId, webhookRequest.WebhookLogId);
 
-            if (entry == null)
+            if (entry == null || !Uri.TryCreate(entry.Config.Uri, UriKind.Absolute, out var configUri))
             {
+                return;
+            }
+
+            var addresses = await GetHostAddressesAsync(configUri);
+            if (addresses is not { Length: > 0})
+            {
+                entry.Config.Enabled = false;
+                await dbWorker.UpdateWebhookConfig(entry.Config, true);
+                messageService.SendHeadersMessage(MessageAction.WebhookUpdated, MessageTarget.Create(entry.ConfigId), null, $"{entry.Config.Name} (DNS resolution failed for {configUri.Host})");
+                return;
+            }
+
+            if (IsUriBlacklisted(addresses))
+            {
+                await dbWorker.RemoveWebhookConfigAsync(entry.ConfigId);
+                messageService.SendHeadersMessage(MessageAction.WebhookDeleted, MessageTarget.Create(entry.ConfigId), null, $"{entry.Config.Name} (blacklist)");
                 return;
             }
 
@@ -109,7 +125,7 @@ public class WebhookSender(
 
                 var response = await pipeline.ExecuteAsync(async context =>
                 {
-                    var request = new HttpRequestMessage(HttpMethod.Post, entry.Config.Uri);
+                    var request = new HttpRequestMessage(HttpMethod.Post, configUri);
 
                     var retryCount = context.Properties.GetValue(RetryCountPropKey, 0);
 
@@ -207,7 +223,26 @@ public class WebhookSender(
         }
     }
 
-    private string GetSecretHash(string secretKey, string body)
+    private async Task<IPAddress[]> GetHostAddressesAsync(Uri uri)
+    {
+        IPAddress[] addresses = null;
+        try
+        {
+            addresses = await Dns.GetHostAddressesAsync(uri.Host);
+        }
+        catch (Exception e)
+        {
+            logger.ErrorWithException($"DNS resolution failed for {uri.Host}", e);
+        }
+        return addresses;
+    }
+
+    private bool IsUriBlacklisted(IPAddress[] addresses)
+    {
+        return addresses.Any(a => settings.Blacklist.Any(r => IPAddressRange.MatchIPs(a.ToString(), r)));
+    }
+
+    private static string GetSecretHash(string secretKey, string body)
     {
         var secretBytes = Encoding.UTF8.GetBytes(secretKey);
 
