89a211fae580a521c9273c2dcdb4a5c2485ac8bc
pavel.bannov <pavel.bannov@onlyoffice.com>
2026-04-13 17:09:47 +0300

    fix bug 79368

--- products/ASC.People/Server/Api/GroupsController.cs
+++ products/ASC.People/Server/Api/GroupsController.cs
@@ -1,25 +1,25 @@
 ﻿// (c) Copyright Ascensio System SIA 2009-2026
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -292,12 +292,12 @@ public class GroupController(
         var anyValidMembers = await inDto.Members.Members
             .ToAsyncEnumerable()
             .AnyAsync(async (userId, _) => await ValidateUserAsync(userId));
-        
+
         if (!anyValidMembers)
         {
             throw new ArgumentException(nameof(inDto.Members.Members));
         }
-        
+
         await RemoveMembersFrom(new MembersRequestDto { Id = inDto.Id, Members = new MembersRequest { Members = (await userManager.GetUsersByGroupAsync(inDto.Id)).Select(x => x.Id) } });
         await AddMembersTo(inDto);
 
@@ -394,7 +394,7 @@ public class GroupController(
 
     private async Task TransferUserToDepartmentAsync(Guid userId, GroupInfo group, bool setAsManager)
     {
-        if (await ValidateUserAsync(userId))
+        if (!await ValidateUserAsync(userId))
         {
             return;
         }
@@ -410,10 +410,10 @@ public class GroupController(
     private async Task<bool> ValidateUserAsync(Guid userId)
     {
         var user = await userManager.GetUsersAsync(userId);
-        return userId == Guid.Empty || 
-               !userManager.UserExists(user) || 
-               user.Status == EmployeeStatus.Terminated || 
-               await userManager.IsGuestAsync(userId);
+        return userId != Guid.Empty &&
+               userManager.UserExists(user) &&
+               user.Status != EmployeeStatus.Terminated &&
+               !await userManager.IsGuestAsync(userId);
     }
 
     private async Task RemoveUserFromDepartmentAsync(Guid userId, GroupInfo group)
@@ -527,8 +527,8 @@ public class GroupControllerAdditional<T>(
         var offset = inDto.StartIndex;
         var count = inDto.Count;
         var text = inDto.Text;
-        
-        var parentUserIds = await fileSharing.GetPureSharesAsync(fileEntry, ShareFilterType.Group, null, inDto.Text, 0, int.MaxValue).Select(r=> r.Id).ToListAsync();
+
+        var parentUserIds = await fileSharing.GetPureSharesAsync(fileEntry, ShareFilterType.Group, null, inDto.Text, 0, int.MaxValue).Select(r => r.Id).ToListAsync();
         var securityDao = daoFactory.GetSecurityDao<T>();
 
         var totalGroups = await securityDao.GetGroupsWithSharedCountAsync(fileEntry, text, inDto.ExcludeShared ?? false, parentUserIds);
