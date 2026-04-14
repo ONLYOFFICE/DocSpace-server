9baae94865ed8e4fc7104a7d01b5aab8dfc0d353
Daniil Kapasovskiy <daniil.kapasovckiy@onlyoffice.com>
2026-04-14 11:43:48 +0300

    Add: new SDK generation

--- common/Tools/ASC.Api.Documentation/ASC.Api.Documentation/Program.cs
+++ common/Tools/ASC.Api.Documentation/ASC.Api.Documentation/Program.cs
@@ -1,4 +1,4 @@
-﻿// (c) Copyright Ascensio System SIA 2009-2026
+// (c) Copyright Ascensio System SIA 2009-2026
 //
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
@@ -24,7 +24,136 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-using ASC.Api.Documentation;
+using System.Diagnostics;
+using System.Text;
 
-var app = new CommandApp<OpenapiJoiner>();
-app.Run(args);
+namespace ASC.Api.Documentation;
+
+internal class Program
+{
+    static int Main(string[] args)
+    {
+        Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+
+        var workDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "SDK"));
+
+        var options = new[]
+        {
+            "CSharp",
+            "Python",
+            "PostmanCollection",
+            "TypeScript",
+            "Java",
+            "Kotlin",
+            "Php",
+            "Swift6",
+            "Go",
+            "Ruby"
+        };
+
+        if (!Directory.Exists(workDirectory))
+        {
+            AnsiConsole.MarkupLine($"[red]Directory not found: {Markup.Escape(workDirectory)}[/]");
+            return 1;
+        }
+
+        AnsiConsole.MarkupLine("[yellow]Joining OpenAPI documents...[/]");
+        var joinExitCode = new CommandApp<OpenapiJoiner>().Run(Array.Empty<string>());
+        if (joinExitCode != 0)
+        {
+            AnsiConsole.MarkupLine($"[red]Join failed with exit code {joinExitCode}.[/]");
+            return joinExitCode;
+        }
+
+        var firstWindowChoice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Select")
+                .AddChoices(new[]
+                {
+                    "Build All SDK",
+                    "Manage Options"
+                })
+        );
+
+        if (firstWindowChoice != "Build All SDK")
+        {
+            while (true)
+            {
+                var selected = AnsiConsole.Prompt(
+                    new MultiSelectionPrompt<string>()
+                        .Title("Manage Options")
+                        .InstructionsText("[grey](Space - Select, Enter - Confirm)[/]")
+                        .NotRequired()
+                        .AddChoices(options)
+                );
+
+                if (selected.Count > 0)
+                {
+                    options = selected.ToArray();
+                    break;
+                }
+
+                AnsiConsole.MarkupLine("[red]You need to select at least one option.[/]");
+            }
+        }
+
+        var commands = new StringBuilder("/c mvn clean package");
+        return RunCommand(workDirectory, commands, options);
+    }
+
+    private static int RunCommand(string workDirectory, StringBuilder commands, IEnumerable<string> options)
+    {
+        foreach (var option in options)
+        {
+            commands.Append($" && openapi-generator-cli generate -c tools/tools{option}.json --custom-generator target/sdk-1.0-jar-with-dependencies.jar");
+
+            if (option == "CSharp")
+            {
+                commands.Append(" && cd ../../../../../sdk/docspace-api-sdk-csharp");
+                commands.Append(" && dotnet build");
+                commands.Append(" && xcopy \"src\\DocSpace.API.SDK\\bin\\Debug\\*.nupkg\" \"..\\..\\.nuget\\packages\" /s /y /b /i");
+                commands.Append(" && cd ../../common/Tools/ASC.Api.Documentation/ASC.Api.Documentation/SDK");
+            }
+
+            if (option == "TypeScript")
+            {
+                commands.Append(" && cd ../../../../../sdk/docspace-api-sdk-typescript");
+                commands.Append(" && npm install");
+                commands.Append(" && npm pack");
+                commands.Append(" && xcopy \"onlyoffice-docspace-api-sdk-*.tgz\" \"..\\..\\..\\client\\libs\\ui-kit\" /s /y /b /i");
+                commands.Append(" && cd ../../common/Tools/ASC.Api.Documentation/ASC.Api.Documentation/SDK");
+            }
+        }
+
+        AnsiConsole.MarkupLine("[yellow]Starting SDK generation[/]");
+        AnsiConsole.MarkupLine($"[grey]Working directory: {Markup.Escape(Path.GetFullPath(workDirectory))}[/]");
+
+        using var process = Process.Start(new ProcessStartInfo("cmd.exe", commands.ToString())
+        {
+            WorkingDirectory = workDirectory,
+            UseShellExecute = false
+        });
+
+        if (process == null)
+        {
+            AnsiConsole.MarkupLine("[red]Failed to start command process.[/]");
+            return 1;
+        }
+
+        process.WaitForExit();
+
+        if (process.ExitCode == 0)
+        {
+            AnsiConsole.MarkupLine("[green]SDK generation completed successfully.[/]");
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[red]SDK generation failed with exit code {process.ExitCode}.[/]");
+        }
+
+        AnsiConsole.MarkupLine("[grey]Press any key to continue...[/]");
+        Console.ReadKey(true);
+
+        return process.ExitCode;
+    }
+}
