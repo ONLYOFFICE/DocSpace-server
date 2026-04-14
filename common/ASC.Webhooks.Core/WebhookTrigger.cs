a8503d5168ca7271d5bd93d9088ad61580aa472f
Andrey Savihin <andrey.savihin@onlyoffice.com>
2026-04-14 13:28:36 +0300

    WebhookTrigger: Replace enum member value with its bit-shift equivalent

--- common/ASC.Webhooks.Core/WebhookTrigger.cs
+++ common/ASC.Webhooks.Core/WebhookTrigger.cs
@@ -1,25 +1,25 @@
 ﻿// (c) Copyright Ascensio System SIA 2009-2026
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -38,16 +38,16 @@ public enum WebhookTrigger : long
     #region User
 
     [Description("user.created")]
-    UserCreated = 1,
+    UserCreated = 1L << 0,
 
     [Description("user.invited")]
-    UserInvited = 2,
+    UserInvited = 1L << 1,
 
     [Description("user.updated")]
-    UserUpdated = 4,
+    UserUpdated = 1L << 2,
 
     [Description("user.deleted")]
-    UserDeleted = 8,
+    UserDeleted = 1L << 3,
 
     #endregion User
 
@@ -55,13 +55,13 @@ public enum WebhookTrigger : long
     #region Group
 
     [Description("group.created")]
-    GroupCreated = 16,
+    GroupCreated = 1L << 4,
 
     [Description("group.updated")]
-    GroupUpdated = 32,
+    GroupUpdated = 1L << 5,
 
     [Description("group.deleted")]
-    GroupDeleted = 64,
+    GroupDeleted = 1L << 6,
 
     #endregion
 
@@ -69,28 +69,28 @@ public enum WebhookTrigger : long
     #region File
 
     [Description("file.created")]
-    FileCreated = 128,
+    FileCreated = 1L << 7,
 
     [Description("file.uploaded")]
-    FileUploaded = 256,
+    FileUploaded = 1L << 8,
 
     [Description("file.updated")]
-    FileUpdated = 512,
+    FileUpdated = 1L << 9,
 
     [Description("file.trashed")]
-    FileTrashed = 1024,
+    FileTrashed = 1L << 10,
 
     [Description("file.deleted")]
-    FileDeleted = 2048,
+    FileDeleted = 1L << 11,
 
     [Description("file.restored")]
-    FileRestored = 4096,
+    FileRestored = 1L << 12,
 
     [Description("file.copied")]
-    FileCopied = 8192,
+    FileCopied = 1L << 13,
 
     [Description("file.moved")]
-    FileMoved = 16384,
+    FileMoved = 1L << 14,
 
     #endregion
 
@@ -98,25 +98,25 @@ public enum WebhookTrigger : long
     #region Folder
 
     [Description("folder.created")]
-    FolderCreated = 32768,
+    FolderCreated = 1L << 15,
 
     [Description("folder.updated")]
-    FolderUpdated = 65536,
+    FolderUpdated = 1L << 16,
 
     [Description("folder.trashed")]
-    FolderTrashed = 131072,
+    FolderTrashed = 1L << 17,
 
     [Description("folder.deleted")]
-    FolderDeleted = 262144,
+    FolderDeleted = 1L << 18,
 
     [Description("folder.restored")]
-    FolderRestored = 524288,
+    FolderRestored = 1L << 19,
 
     [Description("folder.copied")]
-    FolderCopied = 1048576,
+    FolderCopied = 1L << 20,
 
     [Description("folder.moved")]
-    FolderMoved = 2097152,
+    FolderMoved = 1L << 21,
 
     #endregion
 
@@ -124,22 +124,22 @@ public enum WebhookTrigger : long
     #region Room
 
     [Description("room.created")]
-    RoomCreated = 4194304,
+    RoomCreated = 1L << 22,
 
     [Description("room.updated")]
-    RoomUpdated = 8388608,
+    RoomUpdated = 1L << 23,
 
     [Description("room.archived")]
-    RoomArchived = 16777216,
+    RoomArchived = 1L << 24,
 
     [Description("room.deleted")]
-    RoomDeleted = 33554432,
+    RoomDeleted = 1L << 25,
 
     [Description("room.restored")]
-    RoomRestored = 67108864,
+    RoomRestored = 1L << 26,
 
     [Description("room.copied")]
-    RoomCopied = 134217728,
+    RoomCopied = 1L << 27,
 
     #endregion
 
@@ -147,13 +147,13 @@ public enum WebhookTrigger : long
     #region Forms
 
     [Description("form.submit")]
-    FormSubmit = 268435456,
+    FormSubmit = 1L << 28,
 
     [Description("form.filled.out")]
-    FormFilledOut = 536870912,
+    FormFilledOut = 1L << 29,
 
     [Description("form.stopped")]
-    FormStopped = 1073741824,
+    FormStopped = 1L << 30,
 
     #endregion
 
@@ -161,13 +161,13 @@ public enum WebhookTrigger : long
     #region Agent
 
     [Description("agent.created")]
-    AgentCreated = 2147483648,
+    AgentCreated = 1L << 31,
 
     [Description("agent.updated")]
-    AgentUpdated = 4294967296,
+    AgentUpdated = 1L << 32,
 
     [Description("agent.deleted")]
-    AgentDeleted = 8589934592,
+    AgentDeleted = 1L << 33,
 
     #endregion
 }
