71069fbcb861d3075f25aaf540aeb14250bae12a
alexeybannov <alexey.bannov@onlyoffice.com>
2026-04-22 14:25:22 +0300

    aspire: change mysql port 33060 -> 33306

--- common/ASC.AppHost/Configuration/ConnectionStringManager.cs
+++ common/ASC.AppHost/Configuration/ConnectionStringManager.cs
@@ -63,7 +63,7 @@ public class ConnectionStringManager(IDistributedApplicationBuilder builder, str
         var mysqlRootPassword = builder.AddParameter("mysql-root-password", "root", secret: true);
 
         var mysqlResourceBuilder = builder.AddMySql("mysql", password: mysqlRootPassword)
-            .WithEndpoint("tcp", endpoint => endpoint.Port = 33060);
+            .WithEndpoint("tcp", endpoint => endpoint.Port = 33306);
 
         if (withDataVolume)
         {
