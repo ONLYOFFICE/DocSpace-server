9e6466003e424193323fe6b4b08560907d052dc8
pavel.bannov <pavel.bannov@onlyoffice.com>
2026-05-15 18:43:29 +0300

    Files: Add OpenAPI documentation and examples for encryption endpoints
    - Add summary and description for file encryption info endpoints (get/set)
    - Add response descriptions and error codes (400, 403, 404)
    - Add example values for encryption key parameters (id, type, version, publicKey, privateKeyEnc)
    - Add `id` field to `EncryptionKeyRequestDto` schema
    - Update privacy room test to use `AspireAppFixture` and fix API method names
    - Fix whitespace formatting in test files

--- products/ASC.Files/Tests/Tests/08_Private/PrivacyRoomTest.cs
+++ products/ASC.Files/Tests/Tests/08_Private/PrivacyRoomTest.cs
@@ -1,25 +1,25 @@
 ﻿// (c) Copyright Ascensio System SIA 2009-2025
-// 
+//
 // This program is a free software product.
 // You can redistribute it and/or modify it under the terms
 // of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
 // Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
 // to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
 // any third-party rights.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
 // the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
-// 
+//
 // The  interactive user interfaces in modified source and object code versions of the Program must
 // display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
-// 
+//
 // Pursuant to Section 7(b) of the License you must retain the original Product logo when
 // distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
 // trademark law for use of our trademarks.
-// 
+//
 // All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
@@ -36,12 +36,7 @@ namespace ASC.Files.Tests.Tests._08_Privacy;
 [Collection("Test Collection")]
 [Trait("Category", "CRUD")]
 [Trait("Feature", "PrivacyRoom")]
-public class PrivacyRoomTest(
-    FilesApiFactory filesFactory, 
-    WepApiFactory apiFactory, 
-    PeopleFactory peopleFactory,
-    FilesServiceFactory filesServiceProgram) 
-    : BaseTest(filesFactory, apiFactory, peopleFactory, filesServiceProgram)
+public class PrivacyRoomTest(AspireAppFixture fixture) : BaseTest(fixture)
 {
     [Fact]
     public async Task CRUDUserPrivateKey()
@@ -50,20 +45,20 @@ public class PrivacyRoomTest(
 
         var (publicKey, privateKey, _) = ExportPublicAndPrivateKeys();
 
-        var keys = (await _privacyroomApi.SetKeysAsync(new EncryptionKeyRequestDto(publicKey, privateKey), cancellationToken: TestContext.Current.CancellationToken)).Response;
+        var keys = (await _privacyRoomApi.SetKeysAsync(new EncryptionKeyRequestDto(Guid.Empty, publicKey, privateKey), cancellationToken: TestContext.Current.CancellationToken)).Response;
 
         keys.Should().NotBeNull();
         keys.Should().HaveCount(1);
         keys[0].PublicKey.Should().BeEquivalentTo(publicKey);
         keys[0].PrivateKeyEnc.Should().BeEquivalentTo(privateKey);
 
-        keys = (await _privacyroomApi.GetUserKeysAsync(cancellationToken: TestContext.Current.CancellationToken)).Response;
+        keys = (await _privacyRoomApi.GetUserKeysAsync(cancellationToken: TestContext.Current.CancellationToken)).Response;
         keys.Should().NotBeNull();
         keys.Should().HaveCount(1);
         keys[0].PublicKey.Should().BeEquivalentTo(publicKey);
         keys[0].PrivateKeyEnc.Should().BeEquivalentTo(privateKey);
 
-        keys = (await _privacyroomApi.DeleteKeysAsync(keys[0].Id, cancellationToken: TestContext.Current.CancellationToken)).Response;
+        keys = (await _privacyRoomApi.DeleteKeysAsync(keys[0].Id, cancellationToken: TestContext.Current.CancellationToken)).Response;
         keys.Should().NotBeNull();
         keys.Should().BeEmpty();
     }
@@ -72,24 +67,24 @@ public class PrivacyRoomTest(
     public async Task SetFileAccess()
     {
         await _filesClient.Authenticate(Initializer.Owner);
-        
+
         var settings = (await _filesSettingsApi.GetFilesSettingsAsync(TestContext.Current.CancellationToken)).Response;
         var (userPublicKey, userPrivateKeyEnc, userPassword) = ExportPublicAndPrivateKeys();
-        
-        var userKeys = (await _privacyroomApi.SetKeysAsync(new EncryptionKeyRequestDto(userPublicKey, userPrivateKeyEnc), cancellationToken: TestContext.Current.CancellationToken)).Response;
+
+        var userKeys = (await _privacyRoomApi.SetKeysAsync(new EncryptionKeyRequestDto(Guid.Empty, userPublicKey, userPrivateKeyEnc), cancellationToken: TestContext.Current.CancellationToken)).Response;
         var userKey = userKeys[0];
-        
+
         var createRequest = new CreateRoomRequestDto(
             title: "private room",
             roomType: RoomType.CustomRoom,
             @private: true
         );
-        
+
         var createdRoom = (await _roomsApi.CreateRoomAsync(createRequest, TestContext.Current.CancellationToken)).Response;
-        var roomKeys = (await _privacyroomApi.GetUserKeysForRoomAsync(createdRoom.Id, cancellationToken: TestContext.Current.CancellationToken)).Response;
+        var roomKeys = (await _privacyRoomApi.GetUserKeysForRoomAsync(createdRoom.Id, cancellationToken: TestContext.Current.CancellationToken)).Response;
         roomKeys.Should().NotBeNull();
         roomKeys.Should().HaveCount(1);
-        
+
         var aesKeyForEncrypt = RandomNumberGenerator.GetBytes(32);
         var filePrivateKeyEnc = EncryptFilePassword(userPublicKey, aesKeyForEncrypt);
 
@@ -104,29 +99,29 @@ public class PrivacyRoomTest(
         var chunkNumber = 1;
         int bytesRead;
         encryptTempStream.Position = 0;
-        
+
         while ((bytesRead = await encryptTempStream.ReadAsync(buffer.AsMemory(0, chunkSize), TestContext.Current.CancellationToken)) > 0)
         {
             var chunkStream = new MemoryStream(buffer, 0, bytesRead);
             var fileParameter = new FileParameter(chunkStream);
-            
+
             await _filesOperationsApi.UploadAsyncSessionAsync(createdRoom.Id, createdSession.Id, chunkNumber, fileParameter, TestContext.Current.CancellationToken);
             chunkNumber++;
         }
-        
+
         var resultFile = (await _filesOperationsApi.FinalizeSessionAsync(createdRoom.Id, createdSession.Id, TestContext.Current.CancellationToken)).Response;
 
         var fileId = resultFile.Id;
-        
+
         List<AccessRequestKeyDto> keys = [new(roomKeys[0].UserId, roomKeys[0].Id, filePrivateKeyEnc)];
         await _filesApi.SetEncryptionInfoAsync(fileId, keys, cancellationToken: TestContext.Current.CancellationToken);
-        
+
         var result = (await _filesApi.GetEncryptionInfoAsync(fileId, cancellationToken: TestContext.Current.CancellationToken)).Response;
         result.Should().NotBeNull();
         result.UserKeys.Should().NotBeNullOrEmpty();
         result.UserKeys[0].UserId.Should().Be(userKey.UserId);
         result.UserKeys[0].Id.Should().Be(userKey.Id);
-        
+
         result.FileKeys[0].FileId.Should().Be(fileId);
         result.FileKeys[0].UserId.Should().Be(userKey.UserId);
         result.FileKeys[0].PublicKeyId.Should().Be(userKey.Id);
@@ -134,10 +129,10 @@ public class PrivacyRoomTest(
 
         var aesKeyForDecrypt = DecryptFilePassword(result.FileKeys[0].PrivateKeyEnc, DecryptPrivateEncKey(result.UserKeys[0].PrivateKeyEnc, userPassword));
         aesKeyForDecrypt.Should().BeEquivalentTo(aesKeyForEncrypt);
-        
+
         var configuration = (await _filesApi.GetFileInfoAsync(fileId, cancellationToken: TestContext.Current.CancellationToken)).Response;
         var fileStream = await _filesClient.GetStreamAsync(configuration.ViewUrl, TestContext.Current.CancellationToken);
-        
+
         await using var fileTempStream = new MemoryStream();
         await fileStream.CopyToAsync(fileTempStream, TestContext.Current.CancellationToken);
 
@@ -147,7 +142,7 @@ public class PrivacyRoomTest(
 
         AreStreamsEqual(decryptTempStream, stream).Should().BeTrue();
     }
-    
+
     private static Key ExportPublicAndPrivateKeys()
     {
         using var rsa = RSA.Create(2048);
@@ -156,9 +151,9 @@ public class PrivacyRoomTest(
             PbeEncryptionAlgorithm.Aes256Cbc,
             HashAlgorithmName.SHA256,
             iterationCount: 100000);
-        
+
         var password = Initializer.FakerMember.Generate().Password;
-        
+
         var privateKey = Convert.ToBase64String(rsa.ExportEncryptedPkcs8PrivateKey(password, pbeParameters));
         return new Key(publicKey, privateKey, password);
     }
@@ -195,7 +190,7 @@ public class PrivacyRoomTest(
 
         using var sha256 = SHA256.Create();
         var hash1 = sha256.ComputeHash(stream1);
-    
+
         stream2.Position = 0;
         var hash2 = sha256.ComputeHash(stream2);
 
