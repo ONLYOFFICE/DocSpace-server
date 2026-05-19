f522f8a0d6f988779cf4e336b6c21a94b020aa73
pavel.bannov <pavel.bannov@onlyoffice.com>
2026-05-19 21:56:48 +0300

    Rename LocatedInPrivateRoomAsync to LocatedInPrivateRoom and refactor CryptoEngineId constant
    - Remove "Async" suffix from synchronous method `LocatedInPrivateRoom`
    - Extract magic string to `DefaultCryptoEngineId` constant in EncryptionKeyDto
    - Fix code formatting: normalize whitespace in EncryptionKeyDto license header

--- products/ASC.Files/Core/Core/Entries/EncryptionKeyPairDto.cs
+++ products/ASC.Files/Core/Core/Entries/EncryptionKeyPairDto.cs
@@ -1,34 +1,34 @@
 // Copyright (C) Ascensio System SIA, 2009-2026
-// 
+//
 // This program is a free software product. You can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License (AGPL)
 // version 3 as published by the Free Software Foundation, together with the
 // additional terms provided in the LICENSE file.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied
 // warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. For
 // details, see the GNU AGPL at: https://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA by email at info@onlyoffice.com
 // or by postal mail at 20A-6 Ernesta Birznieka-Upisha Street, Riga,
 // LV-1050, Latvia, European Union.
-// 
+//
 // The interactive user interfaces in modified versions of the Program
 // are required to display Appropriate Legal Notices in accordance with
 // Section 5 of the GNU AGPL version 3.
-// 
+//
 // No trademark rights are granted under this License.
-// 
+//
 // All non-code elements of the Product, including illustrations,
 // icon sets, and technical writing content, are licensed under the
 // Creative Commons Attribution-ShareAlike 4.0 International License:
 // https://creativecommons.org/licenses/by-sa/4.0/legalcode
-// 
+//
 // This license applies only to such non-code elements and does not
 // modify or replace the licensing terms applicable to the Program's
 // source code, which remains licensed under the GNU Affero General
 // Public License v3.
-// 
+//
 // SPDX-License-Identifier: AGPL-3.0-only
 
 namespace ASC.Web.Files.Core.Entries;
@@ -36,7 +36,7 @@ namespace ASC.Web.Files.Core.Entries;
 [EnumExtensions]
 [JsonConverter(typeof(EncryptionKeyTypeConverter))]
 public enum EncryptionKeyType
-{    
+{
     Crypt,
     Sign
 }
@@ -73,7 +73,7 @@ public class EncryptionKeyPairDtoHelper(
     FileSharing fileSharing,
     IDaoFactory daoFactory)
 {
-    
+
     public async Task<List<EncryptionKeyDto>> SetKeyPairAsync(IEnumerable<EncryptionKeyDto> keyPairs, bool replace)
     {
         var userId = authContext.CurrentAccount.ID;
@@ -87,7 +87,7 @@ public class EncryptionKeyPairDtoHelper(
         foreach (var keyPair in keyPairs)
         {
             keyPair.UserId = userId;
-            
+
             var index = currentAddressString.FindIndex(r=> r.Id == keyPair.Id);
             if (index > -1)
             {
@@ -172,7 +172,7 @@ public class EncryptionKeyPairDtoHelper(
     public async Task<List<EncryptionKeyDto>> GetKeyPairForRoomAsync<T>(T roomId)
     {
         var folderDao = daoFactory.GetFolderDao<T>();
-        
+
         var room = await folderDao.GetFolderAsync(roomId);
         if (room == null)
         {
@@ -184,7 +184,7 @@ public class EncryptionKeyPairDtoHelper(
             throw new SecurityException(FilesCommonResource.ErrorMessage_SecurityException_EditFile);
         }
 
-        var locatedInPrivateRoom = room.RootFolderType == FolderType.VirtualRooms && DocSpaceHelper.LocatedInPrivateRoomAsync(room);
+        var locatedInPrivateRoom = room.RootFolderType == FolderType.VirtualRooms && DocSpaceHelper.LocatedInPrivateRoom(room);
 
         if (room.RootFolderType != FolderType.Privacy && !locatedInPrivateRoom)
         {
