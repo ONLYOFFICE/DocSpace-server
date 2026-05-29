73c63a18ecd213f452454ed242f773649928e0e1
pavel.bannov <pavel.bannov@onlyoffice.com>
2026-05-29 16:56:37 +0300

    Add support for testing editor images and configurable version tags
    - Add APP_EDITOR_TEST configuration flag to switch between production and testing editor images
    - Add APP_EDITOR_VERSION configuration to specify editor container tag (defaults to "latest")
    - Update image selection logic to use 4testing variants when APP_EDITOR_TEST is enabled
    - Standardize comment spacing throughout file (// instead of // <space>)

--- common/ASC.AppHost/Configuration/ConnectionStringManager.cs
+++ common/ASC.AppHost/Configuration/ConnectionStringManager.cs
@@ -1,34 +1,34 @@
 // Copyright (C) Ascensio System SIA, 2009-2026
-// 
+//
 // This program is a free software product. You can redistribute it and/or
 // modify it under the terms of the GNU Affero General Public License (AGPL)
 // version 3 as published by the Free Software Foundation, together with the
 // additional terms provided in the LICENSE file.
-// 
+//
 // This program is distributed WITHOUT ANY WARRANTY, without even the implied
 // warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. For
 // details, see the GNU AGPL at: https://www.gnu.org/licenses/agpl-3.0.html
-// 
+//
 // You can contact Ascensio System SIA by email at info@onlyoffice.com
 // or by postal mail at 20A-6 Ernesta Birznieka-Upisha Street, Riga,
 // LV-1050, Latvia, European Union.
-// 
+//
 // The interactive user interfaces in modified versions of the Program
 // are required to display Appropriate Legal Notices in accordance with
 // Section 5 of the GNU AGPL version 3.
-// 
+//
 // No trademark rights are granted under this License.
-// 
+//
 // All non-code elements of the Product, including illustrations,
 // icon sets, and technical writing content, are licensed under the
 // Creative Commons Attribution-ShareAlike 4.0 International License:
 // https://creativecommons.org/licenses/by-sa/4.0/legalcode
-// 
+//
 // This license applies only to such non-code elements and does not
 // modify or replace the licensing terms applicable to the Program's
 // source code, which remains licensed under the GNU Affero General
 // Public License v3.
-// 
+//
 // SPDX-License-Identifier: AGPL-3.0-only
 
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -217,15 +217,18 @@ public class ConnectionStringManager(IDistributedApplicationBuilder builder, str
 
     public ConnectionStringManager AddEditors()
     {
+        var forTesting = builder.Configuration["APP_EDITOR_TEST"] == "true";
         var image = builder.Configuration["APP_EDITION"] switch
         {
-            "enterprise" => "onlyoffice/documentserver-ee",
-            "developer" => "onlyoffice/documentserver-de",
-            _ => "onlyoffice/documentserver"
+            "enterprise" => forTesting ? "onlyoffice/4testing-documentserver-ee" : "onlyoffice/documentserver-ee",
+            "developer" => forTesting ? "onlyoffice/4testing-documentserver-de" : "onlyoffice/documentserver-de",
+            _ => forTesting ? "onlyoffice/4testing-documentserver" : "onlyoffice/documentserver"
         };
 
+        var tag = builder.Configuration["APP_EDITOR_VERSION"] ?? "latest";
+
         EditorResource = builder
-            .AddContainer(Constants.EditorsContainer, image, "latest")
+            .AddContainer(Constants.EditorsContainer, image, tag)
             //TODO:get from config or set for the rest projects
             .WithEnvironment("JWT_ENABLED", "true")
             .WithEnvironment("JWT_SECRET", "secret")
