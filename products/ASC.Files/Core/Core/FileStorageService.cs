435418db07c0525bbe868db660d4d75c1cf9adc7
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2026-08-24 15:40:40 +0300

    fix Bug 83349

--- products/ASC.Files/Core/Core/FileStorageService.cs
+++ products/ASC.Files/Core/Core/FileStorageService.cs
@@ -1006,7 +1006,7 @@ public class FileStorageService //: IFileStorageService
             throw new InvalidOperationException(FilesCommonResource.ErrorMessage_SecurityException);
         }
 
-        if (!isRoom && parent.FolderType == FolderType.VirtualRooms)
+        if (!isRoom && parent.FolderType is FolderType.VirtualRooms or FolderType.Forms)
         {
             throw new InvalidOperationException(FilesCommonResource.ErrorMessage_SecurityException_Create);
         }
@@ -1668,7 +1668,8 @@ public class FileStorageService //: IFileStorageService
                             folder.FolderType != FolderType.VirtualRooms &&
                             folder.FolderType != FolderType.RoomTemplates &&
                             folder.FolderType != FolderType.DefaultTemplates &&
-                            folder.FolderType != FolderType.Archive;
+                            folder.FolderType != FolderType.Archive &&
+                            folder.FolderType != FolderType.Forms;
 
             if (!canCreate)
             {
