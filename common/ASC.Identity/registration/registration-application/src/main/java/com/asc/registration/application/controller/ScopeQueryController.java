bb26036894c366466ac46afa09fe13d571568f74
Dmitrii Vershinin <dmitry.vershinin@onlyoffice.com>
2026-09-10 17:53:06 +0500

    fix(oauth-api): list of scopes schema to docs

--- common/ASC.Identity/registration/registration-application/src/main/java/com/asc/registration/application/controller/ScopeQueryController.java
+++ common/ASC.Identity/registration/registration-application/src/main/java/com/asc/registration/application/controller/ScopeQueryController.java
@@ -38,6 +38,7 @@ import com.asc.registration.service.ports.input.service.ScopeApplicationService;
 import com.asc.registration.service.transfer.response.ScopeResponse;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -98,11 +99,10 @@ public class ScopeQueryController {
             content =
                 @Content(
                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema =
-                        @Schema(
-                            implementation = ScopeResponse.class,
-                            type = "array",
-                            description = "List of OAuth2 scopes"),
+                    array =
+                        @ArraySchema(
+                            schema = @Schema(implementation = ScopeResponse.class),
+                            arraySchema = @Schema(description = "List of OAuth2 scopes")),
                     examples =
                         @ExampleObject(
                             value =
