680df04da80051daffdc53b93bef1ad0d032d719
Nikolay Rechkin <nikolay.rechkin@onlyoffice.com>
2026-09-18 21:21:22 +0300

    restore fallback to persisted form category in editor config

--- products/ASC.Files/Server/ApiModels/ResponseDto/ConfigurationDto.cs
+++ products/ASC.Files/Server/ApiModels/ResponseDto/ConfigurationDto.cs
@@ -736,7 +736,7 @@ public class DocumentConfigConverter<T>(InfoConfigConverter<T> configConverter, 
         };
 
         if (FileUtility.GetFileTypeByExtention(FileUtility.GetFileExtension(file.Title)) == FileType.Pdf
-            && await fileChecker.IsFormPDFFile(file))
+            && (file.IsForm || await fileChecker.IsFormPDFFile(file)))
         {
             result.IsForm = true;
         }
